004fe6f0346fde8f4b20c2a759c2f5f4ce8ba8f1
gwkim <youlive789@gmail.com>
2019-05-01 20:01:25 +0900

    feat: Brute-force search combination

--- BruteForce/combination.cpp
+++ BruteForce/combination.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 // 주어진 수열에서 조합을 가져온다.
 // 문제가 있는 접근법이다. 수열의 크기나,
@@ -34,8 +35,33 @@ void getCombinationByRecursion(
     getCombinationByRecursion(startIndex + 1, sequenceEndIndex, combinationSize); 
 }
 
+// n:전체 원소의 수
+// picked: 지금까지 고른 원소들의 번호
+// toPick: 더 고를 원소의 수
+// 일 때, 앞으로 toPick개의 원소를 고르는 모든 방법을 출력한다.
+void pick(int n, std::vector<int>& picked, int toPick) {
+  // 기저 사례: 더 고를 원소가 없을 때 고른 원소들을 출력한다.
+  if (toPick == 0) { 
+      for (auto number : picked) {
+          std::cout << number << " ";
+      }
+      std::cout << std::endl;
+      return; 
+  }
+  // 고를 수 있는 가장 작은 번호를 계산한다.
+  int smallest = picked.empty() ? 0 : picked.back() + 1;
+  // 이 단계에서 원소 하나를 고른다.
+  for (int next = smallest; next < n; ++next) {
+    picked.push_back(next);
+    pick(n, picked, toPick - 1);
+    picked.pop_back();
+  }
+}
+
 int main() {
 
-    getCombinationByRecursion(0, 5, 4);
+    //getCombinationByRecursion(0, 5, 4);
+    std::vector<int> picked;
+    pick(3, picked, 2);
     return 0;
 }
