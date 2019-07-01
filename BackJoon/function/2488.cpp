85fe550a9c95aa43b6930409cb147c934bcf1c3f
gwkim <youlive789@gmail.com>
2019-07-01 22:45:45 +0900

    feat: Add backjoon io problems

---
+++ BackJoon/function/2488.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    using namespace std;
+
+
+    return 0;
+}
---
+++ BackJoon/io/cat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string cat = "\\    /\\\n";
+    cat.append(")  ( ')\n");
+    cat.append("(  /  )\n");
+    cat.append(" \\(__)|");
+
+    std::cout << cat.c_str() << std::endl;
+    return 0;
+}
