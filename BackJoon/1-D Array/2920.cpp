5b1a8599ec11b46e22f17bb5c17053a9b29a0f36
Geon-Woo Kim <youlive789@gmail.com>
2019-07-05 12:59:58 +0900

    feat: Modify backjoon problem 2920

--- BackJoon/1-D Array/2920.cpp
+++ BackJoon/1-D Array/2920.cpp
@@ -7,18 +7,37 @@ int main() {
 
     const string ASC = "ascending";
     const string DESC = "descending";
-    const string MIX = "mixed";
-    
+    const string MIX = "mixed"; 
+
     int ascCounter = 0;
     int descCounter = 0;
     int melodyCount = 8;
+    int melodyList[melodyCount];
 
     for (int index = 0; index < melodyCount; ++index) {
+        cin >> melodyList[index];
+    }
+
+    for (int index = 1; index < melodyCount; ++index) {
         // 바로 종료조건 - mixed
-        if (ascCounter > 0 && descCounter > 0) {
+        if ( (ascCounter > 0 && descCounter > 0) || (ascCounter == 0 && descCounter == 0) ) {
             cout << MIX << endl;
-            break;
+            return 0;
+        } 
+
+        if (melodyList[index - 1] - melodyList[index] == -1) {
+            ascCounter++;              
+        } 
+        else if (melodyList[index - 1] - melodyList[index] == 1) {
+            descCounter++;     
         }
+    }
+
+    if (ascCounter == 7) {
+        cout << ASC << endl;
+    } 
+    else if (descCounter == 7) {
+        cout << DESC << endl;
     }
 
     return 0;
