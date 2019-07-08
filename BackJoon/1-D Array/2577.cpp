a68c65a5fd08ea73e6bdacb176ec49e552432b8d
gwkim <youlive789@gmail.com>
2019-07-08 22:04:27 +0900

    feat: solved backjoon problem 2577

--- BackJoon/1-D Array/2577.cpp
+++ BackJoon/1-D Array/2577.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
-#include <map>
 
 using namespace std;
 
 int main() {
 
-    map<int, int> numberDict;
+    int numberDict[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int firstNum, secondNum, thirdNum, multiple;
 
-    
+    cin >> firstNum >> secondNum >> thirdNum;
+
+    multiple = firstNum * secondNum * thirdNum;
+    while (multiple != 0) {
+        int check = multiple % 10;
+        multiple /= 10;
+        numberDict[check]++;
+    }
+
+    for (int index = 0; index < 10; index++) {
+        cout << numberDict[index] << endl;
+    }
 
     return 0;
 }
