709c621cde4936d779a5d53c1cf8c0ec546d89ca
Geon-Woo Kim <youlive789@gmail.com>
2019-07-11 08:03:47 +0900

    feat: Modify Backjoon problem 8958

--- BackJoon/1-D Array/8958.cpp
+++ BackJoon/1-D Array/8958.cpp
@@ -5,12 +5,35 @@ using namespace std;
 
 int main() {
 
-    int sumPoint = 0;
-    int currentPoint = 0;
-
     int numberCase = 0;
     cin >> numberCase;
 
+    string testCase;
+    int sizeTestCase = 0;
+
+    int sumPoint[numberCase];
+    int currentPoint = 0;
+
+    for (int index = 0; index < numberCase; ++index) {
+        
+        cin >> testCase;
+        sizeTestCase = testCase.size();
+        
+        for (int stringIndex = 0; stringIndex < sizeTestCase; ++stringIndex) {
+            if (testCase[stringIndex] == 'O') {
+                ++currentPoint;
+            }
+            else {
+                currentPoint = 0;
+            }
+            sumPoint[index] += currentPoint;
+            currentPoint = 0;
+        }              
+    }
+
+    for (int index = 0; index < numberCase; ++index) {
+        cout << sumPoint[index] << endl;
+    }
 
     return 0;
 }
