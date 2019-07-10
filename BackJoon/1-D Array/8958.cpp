2c10463ffbcd3be58d87f28e0bef4cf138179085
gwkim <youlive789@gmail.com>
2019-07-10 22:23:17 +0900

    feat: Add Backjoon problem 8958

--- BackJoon/1-D Array/8958.cpp
+++ BackJoon/1-D Array/8958.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    //
+    int sumPoint = 0;
+    int currentPoint = 0;
+
+    int numberCase = 0;
+    cin >> numberCase;
+
 
     return 0;
 }
