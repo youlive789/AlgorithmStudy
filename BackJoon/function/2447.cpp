350456ba53223b1a33e4577010cea5441c9c9c40
gwkim <youlive789@gmail.com>
2019-07-24 08:43:18 +0900

    feat: modify backjoon problem 2447

--- BackJoon/function/2447.cpp
+++ BackJoon/function/2447.cpp
@@ -5,10 +5,21 @@ using namespace std;
 // 모양틀 초기화
 bool shape[2187][2187] = {false};
 
+// 모양틀 범위 체크
+bool checkHeight(const int& height, const int& compare) {
+    if (height > compare || height <= compare) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 // 모양 형성
 void makeShape(const int& height, const int& x, const int& y) {
 
-    if (height == 0) return;
+    // x와 y 좌표가 모양틀을 벗어난다면 종료
+    if (!checkHeight(height, x) && !checkHeight(height, y)) return;
 
     shape[y][x] = true;
     shape[y][x+1] = true;
@@ -19,8 +30,7 @@ void makeShape(const int& height, const int& x, const int& y) {
     shape[y+2][x+1] = true;
     shape[y+2][x+2] = true;
 
-    makeShape(height / 3, x + 3, y);
-    
+    makeShape(height, x + 3, y - 3);
 }
 
 int main() {
@@ -29,10 +39,8 @@ int main() {
     int height = 0;
     cin >> height;
 
-    for (int index = 0; index < height / 3; ++index) {
-        makeShape(height, 0, index * 3);
-    }
-
+    makeShape(height, 0, height);
+    
     for (int yIndex = 0; yIndex < height; ++yIndex) {
         for (int xIndex = 0; xIndex < height; ++xIndex) {
             // 별 프린트
