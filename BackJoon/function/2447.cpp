d855a75de00be0742050ea18a977d16876cd1d10
gwkim <youlive789@gmail.com>
2019-07-25 08:39:13 +0900

    feat: Modify backjoon problem 2447

--- BackJoon/function/2447.cpp
+++ BackJoon/function/2447.cpp
@@ -7,7 +7,7 @@ bool shape[2187][2187] = {false};
 
 // 모양틀 범위 체크
 bool checkHeight(const int& height, const int& compare) {
-    if (height > compare || height <= compare) {
+    if (height <= compare) {
         return false;
     }
     else {
@@ -16,10 +16,14 @@ bool checkHeight(const int& height, const int& compare) {
 }
 
 // 모양 형성
-void makeShape(const int& height, const int& x, const int& y) {
+void makeShape(const int& height, int x, int y) {
 
     // x와 y 좌표가 모양틀을 벗어난다면 종료
     if (!checkHeight(height, x) && !checkHeight(height, y)) return;
+    if (!checkHeight(height, x)) { 
+        x = 0;
+        y += 3;
+    }
 
     shape[y][x] = true;
     shape[y][x+1] = true;
@@ -30,7 +34,7 @@ void makeShape(const int& height, const int& x, const int& y) {
     shape[y+2][x+1] = true;
     shape[y+2][x+2] = true;
 
-    makeShape(height, x + 3, y - 3);
+    makeShape(height, x + 3, y);
 }
 
 int main() {
@@ -39,7 +43,7 @@ int main() {
     int height = 0;
     cin >> height;
 
-    makeShape(height, 0, height);
+    makeShape(height, 0, 0);
     
     for (int yIndex = 0; yIndex < height; ++yIndex) {
         for (int xIndex = 0; xIndex < height; ++xIndex) {
