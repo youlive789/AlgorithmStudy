c354f5c2667bec4b632376518c94a7e71041645d
Geon-Woo Kim <youlive789@gmail.com>
2019-07-26 08:18:07 +0900

    feat: modify backjoon problem 2447

--- BackJoon/function/2447.cpp
+++ BackJoon/function/2447.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 모양틀 초기화
 bool shape[2187][2187] = {false};
 
-// 모양틀 범위 체크
+// 모양틀 범위를 체크한다.
 bool checkHeight(const int& height, const int& compare) {
     if (height <= compare) {
         return false;
@@ -15,15 +15,14 @@ bool checkHeight(const int& height, const int& compare) {
     }
 }
 
-// 모양 형성
+// 모양을 형성한다.
 void makeShape(const int& height, int x, int y) {
 
-    // x와 y 좌표가 모양틀을 벗어난다면 종료
+    // x와 y 좌표가 모양틀을 벗어난다면 종료한다.
     if (!checkHeight(height, x) && !checkHeight(height, y)) return;
-    if (!checkHeight(height, x)) { 
-        x = 0;
-        y += 3;
-    }
+
+    // 한 줄을 다 찍었으면 다음줄을 찍기 시작한다.
+    if (!checkHeight(height, x)) { x = 0; y += 3;}
 
     shape[y][x] = true;
     shape[y][x+1] = true;
@@ -43,6 +42,7 @@ int main() {
     int height = 0;
     cin >> height;
 
+    // 반복문 없이 모양틀 생성
     makeShape(height, 0, 0);
     
     for (int yIndex = 0; yIndex < height; ++yIndex) {
