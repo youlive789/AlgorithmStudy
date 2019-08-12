91944ce9df30d84745981cc2137690519a6aa6f1
Geon-Woo Kim <youlive789@gmail.com>
2019-08-13 08:54:03 +0900

    feat: remind backjoon problem 2447

--- BackJoon/function/2447.py
+++ BackJoon/function/2447.py
@@ -1,6 +1,19 @@
 
-def star(height) :
-    pass
+def star(height, x, y) :
+    if height == 0 :
+        return
+
+    if int(x / height) % 3 == 1 and int(y / height) % 3 == 1 :
+        print(" ", end='')
+    else:
+        if int(height / 3) == 0  :
+            print("*", end='')
+        else :
+            star(height / 3, x, y)
 
 if __name__ == "__main__":
-    pass
+    N = 27
+    for x in range(N) :
+        for y in range(N) :
+            star(N, x, y)
+        print("\n", end='')
