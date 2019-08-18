db710870a532c91c116de7156cf9069bc574d80d
gwkim <youlive789@gmail.com>
2019-08-18 10:28:03 +0900

    feat: More understandable solution for backjoon problem 2447

--- BackJoon/function/2447.cpp
+++ BackJoon/function/2447.cpp
@@ -2,27 +2,65 @@
 
 using namespace std;
 
-void star(int modN, int i,int j)
+char mat[2201][2201];
+ 
+void solve(int y, int x, int num)
 {
-  if( (i / modN) % 3 == 1 && (j / modN) % 3 == 1)
-      cout << " ";
-  else
-  {
-      if(modN/3 == 0)
-          cout << "*";
-      else
-          star(modN/3,i,j);
-  }
+    if(num == 1)
+    {
+        mat[y][x]='*';
+        return;
+    }
+     
+    int div = num/3;
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            if(i == 1 && j == 1)
+                ;
+            else
+                solve(y+(i*div), x+(j*div), div);
+        }
+    }
 }
-int main() {
-  int N;
-  cin>>N;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+     
+    memset(mat, ' ', sizeof(mat));
+     
+    solve(0, 0, n);
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+            printf("%c", mat[i][j]);
+        printf("\n");
+    }
+}
 
-  for(int i=0;i<N;i++)
-  {
-      for(int j=0;j<N;j++)
-          star(N,i,j);
-      cout << endl;
-  }
-  return 0;
-}
+// void star(int modN, int i,int j)
+// {
+//   if( (i / modN) % 3 == 1 && (j / modN) % 3 == 1)
+//       cout << " ";
+//   else
+//   {
+//       if(modN/3 == 0)
+//           cout << "*";
+//       else
+//           star(modN/3,i,j);
+//   }
+// }
+// int main() {
+//   int N;
+//   cin>>N;
+
+//   for(int i=0;i<N;i++)
+//   {
+//       for(int j=0;j<N;j++)
+//           star(N,i,j);
+//       cout << endl;
+//   }
+//   return 0;
+// }
