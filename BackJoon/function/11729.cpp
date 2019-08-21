2e0925bcf20f9f7bea22e6087266eaae36332ba1
Geon-Woo Kim <youlive789@gmail.com>
2019-08-22 08:54:06 +0900

    feat: Modify backjoon problem 11729

--- BackJoon/function/11729.cpp
+++ BackJoon/function/11729.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
-stack<int> first;
-stack<int> second;
-stack<int> third;
+void popDisk(stack<int> &origin, stack<int> &target) {
 
-void popDisk(stack<int> origin, stack<int> target) {
-    if (origin.top() < target.top() || target.empty()) {
-        int disk = origin.top();
-        origin.pop();
-        target.push(disk);
-    }
+    int disk = origin.top();
+    origin.pop();
+    target.push(disk);
     return;
 }
 
-void moveDisk(stack<int> first, stack<int> second, stack<int> third) {
+void moveDisk(stack<int> &first, stack<int> &second, stack<int> &third) {
     // first와 second에 디스크가 없으면 종료
     if (first.empty() && second.empty()) return;
 
-    popDisk(first, second);
-    popDisk(first, third);
-    popDisk(second, first);
-    popDisk(second, third);
-    popDisk(third, first);
-    popDisk(third, second);
-
-    moveDisk(first, second, third);
+     moveDisk(first, second, third);
 }
 
 int main() {
+
+    stack<int> first;
+    stack<int> second;
+    stack<int> third;
 
     int numDisk;
     cin >> numDisk;
