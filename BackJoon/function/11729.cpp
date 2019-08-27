049f89f43e4f46c11a924760876cdb9a3a0d45d1
Geon-Woo Kim <youlive789@gmail.com>
2019-08-28 08:54:36 +0900

    feat: modify backjoon problem 11729

--- BackJoon/function/11729.cpp
+++ BackJoon/function/11729.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <stack>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 void popDisk(stack<int> &origin, stack<int> &target) {
-
-    int disk = origin.top();
-    origin.pop();
-    target.push(disk);
+    if (origin.top() < target.top() || target.empty()) {
+        int disk = origin.top();
+        origin.pop();
+        target.push(disk);
+    } 
     return;
 }
 
@@ -16,11 +18,24 @@ void moveDisk(stack<int> &first, stack<int> &second, stack<int> &third) {
     // first와 second에 디스크가 없으면 종료
     if (first.empty() && second.empty()) return;
 
-     moveDisk(first, second, third);
+    popDisk(first, second);
+
+    //moveDisk(first, second, third);
 }
 
-void printDisk() {
-    
+void printDisk(const stack<int> & disks) {
+    stack<int> tmp = disks;
+
+    if (tmp.size() <= 0) {
+        cout << "Nothing in here." << endl;
+    }
+
+    while(!tmp.empty()) {
+        cout << tmp.top() << endl;
+        tmp.pop();
+    }
+
+    return;
 }
 
 int main() {
@@ -39,6 +54,17 @@ int main() {
 
     moveDisk(first, second, third);
 
-    cout << "done" << endl;
+    cout << "Fisrt disk" << endl;
+    printDisk(first);
+    cout << endl;
+
+    cout << "Second disk" << endl;
+    printDisk(second);
+    cout << endl;
+
+    cout << "Third disk" << endl;
+    printDisk(third);
+    cout << endl;
+
     return 0;
 }
