cb61a90bfdd328dee239209fd7aaa4abc263b4d0
gwkim <youlive789@gmail.com>
2020-02-21 13:43:25 +0900

    Feat: Have to find table method to backjoon problem 2292

--- BackJoon/math1/2292.cpp
+++ BackJoon/math1/2292.cpp
@@ -3,21 +3,7 @@
 
 using namespace std;
 
-int getNextTableNumber(const int& index, const int& firstNum, const int& firstDiff) {
-    if (index < 3) throw logic_error("인덱스는 3보다 커야합니다!");
-    return firstNum + ((index - 1) * ((2 * firstDiff) + (index - 2) * 6) / 2);
-}
-
 int main() {
-
-    int tableSize;
-    cin >> tableSize;
-
-    int tableRows = static_cast<int>(ceil(static_cast<double>(tableSize) / 6));
-
-    for (int tableIndex = 2; tableIndex < tableSize; tableIndex++) {
-    
-    }
 
     return 0;
 }
