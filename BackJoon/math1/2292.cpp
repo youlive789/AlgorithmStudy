3de4c1b787a66e71b94f4a9caedb0eebc989f156
gwkim <youlive789@gmail.com>
2020-02-21 16:26:18 +0900

    feat: Fail backjoon problem 2292

--- BackJoon/math1/2292.cpp
+++ BackJoon/math1/2292.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main() {
+
+    long long n;
+    cin >> n;
+
+    int cnt = 1;
+    long long range = 1;
+    long long tmp = 1;
+
+    while (true) {
+        if (range >= n) {
+            break;
+        }
+        tmp = 6 * (cnt++);
+        range += tmp;
+    }
+
+    cout << cnt;
 
     return 0;
 }
