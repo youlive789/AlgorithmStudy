77e908fd500ad8c25a1e4c8d9877a686857d410f
Geon-Woo Kim <youlive789@gmail.com>
2020-02-24 13:09:59 +0900

    feat: Have to fix backjoon problem 1193

--- BackJoon/math1/1193.cpp
+++ BackJoon/math1/1193.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-/*
-1/1 2/1 1/2 1/3 2/2 3/1
+    int inputNumber;
+    cin >> inputNumber;
 
-*/
+    int layer = static_cast<int>(ceil( (-1 + sqrt(1 + 8 * inputNumber)) / 2 ));
+    int layerCnt = layer * (layer + 1) / 2;
+    int numberIndex = layerCnt - inputNumber;
+
+    int x, y = -999;
+    bool layerEven = ((layer % 2) == 0);
+
+    if (layerEven) {
+        x = 1;
+        y = layer;
+        while (numberIndex != 0) {
+            x++;
+            y--;
+            numberIndex--;
+        }
+    }
+    else {
+        x = layer;
+        y = 1;
+        while (numberIndex != 0) {
+            x--;
+            y++;
+            numberIndex--;
+        }
+    }
+    
+    cout << x << "/" << y << endl;
+
     return 0;
 }
