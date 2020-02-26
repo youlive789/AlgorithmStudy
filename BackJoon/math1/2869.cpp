b9c3d98c93a3d5e0e1f3041b6e75974214be3438
gwkim <youlive789@gmail.com>
2020-02-26 11:15:49 +0900

    feat: Solved backjoon problem 2869

--- BackJoon/math1/2869.cpp
+++ BackJoon/math1/2869.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
 
     int ascentLength, descentLength, targetLength;
-    cin >> ascentLength;
-    cin >> descentLength;
-    cin >> targetLength;
+    cin >> ascentLength >> descentLength >> targetLength;
+
+    if (descentLength >= ascentLength) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int days = (targetLength - descentLength - 1) / (ascentLength - descentLength) + 1;
+    if (days == 0) days++;
+    cout << days << endl;
 
     return 0;
 }
