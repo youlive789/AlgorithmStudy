bf136bd8ab0e5363c599153c058ca33b2fa901e2
gwkim <youlive789@gmail.com>
2020-03-04 12:55:13 +0900

    feat: Fail backjoon 1011

--- BackJoon/math1/1011.cpp
+++ BackJoon/math1/1011.cpp
@@ -3,71 +3,25 @@
 
 using namespace std;
 
-struct Step {
-    int steps;
-    int lengthPass;
-    int lengthLeft;
-    int lastMove;
-};
-
-// Bruteforce
-Step moveStep(Step K) {
-    if (K.lengthLeft <= 0) return K;
-
-    if (K.steps == 0 || K.lengthLeft == 1) {
-        K.lastMove = 1;
-    }
-    else if (K.lastMove < K.lengthLeft && K.lengthLeft > 2) {
-        K.lastMove += 1;
-    }
-    else if (K.lengthPass > K.lengthLeft && K.lengthLeft > 1) {
-        K.lastMove -= 1;
-    }
-
-    K.steps += 1;
-    K.lengthLeft -= K.lastMove;
-    K.lengthPass += K.lastMove;
-
-    moveStep(K);
-}
-
-int getOptimalStep(const int& length) {
-    if (length <= 3) return length;
-    if (length % 2 == 0) {
-        return -2 + ceil(sqrt(1 + 4 * length));
-    }
-    else {
-        return -2 + ceil(sqrt(2 + 4* length));
-    }
-}
-
 int main() {
 
-    int cases;
+    int cases; 
     cin >> cases;
 
     while (cases--) {
         int x, y;
         cin >> x >> y;
-        cout << getOptimalStep(y - x) << endl;
+
+        long length = y - x;
+        long answer = 1;
+        long dis = 0;
+
+        while(length > dis) {
+            answer++;
+            dis += answer / 2;
+        }
+        cout << answer - 1 << endl;
     }
 
     return 0;
 }
-
-/*
-1 1
-2 2
-3 3
-
-4 6     sum(n/2) * 2 = L
-5 9     sum(n/2) * 2 + ceil(n/2) = L
-6 12    
-7 16    
-8 20    
-9 25    
-
-L / 2 = sum(n/2) = (n/2) * (n/2 + 1) / 2
-L = n/2 * (n/2 + 1)
-2L = n^2 + 2n
-*/
