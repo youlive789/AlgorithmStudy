c7f3a0bfbc9ecf871e00645422803c2f84dd71a1
Geon-Woo Kim <youlive789@gmail.com>
2020-03-06 10:48:51 +0900

    feat: Add Backjoon problem 4948

---
+++ BackJoon/math2/4948.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int eratos(const int& size) {
+
+    if (size == 1) return 1;
+
+    bool eratosGrid[2*size + 1] = {true};
+
+    // 에라토스 테네스의 채
+    int i = 2, j = 4;
+    while ( i * i <= 2*size) {
+        if (eratosGrid[i]) {
+            while (j <= 2*size) {
+                eratosGrid[j] = false;
+                j += i;
+            }
+        }
+        i += 1;
+        j = i * 2;
+    }
+
+    int count = 0;
+    for (int index = size; index <= 2*size; index++) {
+        if (eratosGrid[index]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+
+    int inputNumber = 1;
+    while (inputNumber) {
+        cin >> inputNumber;
+
+        cout << eratos(inputNumber) << endl;
+    }
+
+    return 0;
+}
