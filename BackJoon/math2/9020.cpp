fbbdc1e4d924d9e6b9b97a9b2a8f6868e1a49d44
gwkim <youlive789@gmail.com>
2020-03-09 08:48:21 +0900

    feat: Add Backjoon problem 9020

---
+++ BackJoon/math2/9020.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+골드바흐 파티션
+4 = 2 + 2
+6 = 3 + 3
+
+1. 주어진 범위의 에라토스 채를 생성
+2. 두 개의 루프를 순회하면서 골드바흐 파티션 탐색
+*/
+
+int main() {
+
+
+
+    return 0;
+}
