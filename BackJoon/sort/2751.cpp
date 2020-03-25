fd791509c312a2652b3e489d042d06cdb3f194e9
gwkim <youlive789@gmail.com>
2020-03-25 15:15:56 +0900

    feat: Have to study merge, quick sort

--- BackJoon/sort/2751.cpp
+++ BackJoon/sort/2751.cpp
@@ -1,63 +1,79 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 
 using namespace std;
 
-void mergesort(int target[], int size);
-void partition(int target[], int temp[], int start, int end);
-void merge(int target[], int temp[], int start, int half, int end);
+void mergesort(vector<int> & target);
+void partition(vector<int> & target, int start, int end);
+void merge(vector<int> & target, int start, int half, int end);
 
 int main() {
 
-    int testArr[] = {5, 7, 3 , 2, 1, 9, 8, 4};
-    int size = sizeof(testArr)/sizeof(int);
-
-    mergesort(testArr, size);
-
-    for (int idx = 0; idx < size; idx++) {
-        cout << testArr[idx] << " ";
+    int cases;
+    scanf("%d", &cases);
+    vector<int> testArr;
+    while (cases--) {
+        int temp;
+        scanf("%d", &temp);
+        testArr.push_back(temp);
     }
+    
+    mergesort(testArr);
 
+    for (int idx = 0; idx < testArr.size(); idx++) {
+        printf("%d\n", testArr[idx]);
+    }
 
     return 0;
 }
 
-void mergesort(int target[], int size) {
-    int temp[size];
-    partition(target, temp, 0, size - 1);
+void mergesort(vector<int> & target) {
+    partition(target, 0, target.size() - 1);
 }
 
-void partition(int target[], int temp[], int start, int end) {
+void partition(vector<int> & target, int start, int end) {
     if (start < end) {
         int half = (start + end) / 2;
-        partition(target, temp, start, half);
-        partition(target, temp, half+1, end);
-        merge(target, temp, start, half, end);
+        partition(target, start, half);
+        partition(target, half+1, end);
+        merge(target, start, half, end);
     }
 }
 
-void merge(int target[], int temp[], int start, int half, int end) {
+void merge(vector<int> & target, int start, int half, int end) {
     int part1 = start;
     int part2 = half + 1;
     int index = start;
 
-    for (int idx = start; idx < end; idx++) {
-        temp[idx] = target[idx];
-    }
+    int temp[target.size()];
 
     while (part1 <= half && part2 <= end) {
-        if (temp[part1] <= temp[part2]) {
-            target[index] = temp[part1];
+        if (target[part1] < target[part2]) {
+            temp[index] = target[part1];
             part1++;
         }
         else {
-            target[index] = temp[part2];
+            temp[index] = target[part2];
             part2++;
         }
         index++;
     }
 
-    for (int idx = 0; idx <= half - part1; idx++) {
-        target[idx + 1] = temp[part1 + idx];
+    if (part1 > half) {
+        for (int idx = part2; idx <= end; idx++) {
+            temp[index] = target[idx];
+            index++;
+        }
     }
+    else {
+        for (int idx = part1; idx <= half; idx++) {
+            temp[index] = target[idx];
+            index++;
+        }
+    }
+
+    for (int idx = start; idx <= end; idx++) {
+        target[idx] = temp[idx];
+    }
+
 }
