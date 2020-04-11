4a6d0bc044f008da52ba3b4ab40d8c5d8f2a0363
gwkim <youlive789@gmail.com>
2020-04-11 18:13:44 +0900

    feat: Finalizes codeup basic

--- Codeup/basic/8.cpp
+++ Codeup/basic/8.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
 #include <cmath>
 #include <cstdio>
-#include <string>
+#include <string.h>
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
+void move(int arr[][10], int x, int y) {
+    if (arr[y][x] == 2) {
+        arr[y][x] = 9;
+        return;
+    }
+
+    arr[y][x] = 9;
+
+    if (arr[y][x+1] != 1) {
+        move(arr, x+1, y);
+    } 
+    else if (arr[y+1][x] != 1 && arr[y][x+1] != 0) {
+       move(arr, x, y+1);
+    }
+    
+}
+
 int main() {
 
-    long a, d, c;
-    cin >> a >> d >> c;
+    int table[10][10];
 
-    int day = 1;
-    while (day % a != 0 || day % d != 0 || day % c != 0) {
-        day++;
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 10; j ++) {
+            int t;
+            cin >> t;
+            table[i][j] = t;
+        }
     }
-    cout << day << endl;
+
+    move(table, 1, 1);
+
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 10; j ++) {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
