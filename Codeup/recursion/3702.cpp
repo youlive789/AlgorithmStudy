5c2315510e2412a71ae81b374e2a3173a351350a
gwkim <youlive789@gmail.com>
2020-04-28 09:01:39 +0900

    feat: Modify codeup 3702

--- Codeup/recursion/3702.cpp
+++ Codeup/recursion/3702.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int cache[100][100] = {0};
+
 int comb(int n, int r) {
     if (n == r || r == 0) return 1;
-    return comb(n - 1, r) + comb(n - 1, r - 1);
+    if (cache[n][r]) {
+        return cache[n][r];
+    }
+    else {
+        return cache[n][r] = (comb(n - 1, r) + comb(n - 1, r - 1)) % 100000000;
+    }
 }
 
 int main() {
@@ -12,7 +20,11 @@ int main() {
     int r, c;
     cin >> r >> c;
 
-    cout << comb(r+1, c) % 100000000 << endl;
+    int n = r + c - 2;
+    int s = n % 2 == 0 ? 1 : 0;
+    int t = abs(c - n) - s;
+
+    cout << comb(n, t) << endl;
 
     return 0;
 }
