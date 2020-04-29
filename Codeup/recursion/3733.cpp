3bbd8c123f4b6dad095a7ff336f7f803f357fa94
gwkim <youlive789@gmail.com>
2020-04-29 22:00:45 +0900

    feat: Solved codeup 3733 problem

--- Codeup/recursion/3733.cpp
+++ Codeup/recursion/3733.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-unsigned int cache[100000000] = {0};
+int cache[100000000] = {0};
 
-int colatz(int partial_result, long long int n) {
-    if (n == 1) return partial_result;
+int colatz(long long int n) {
+    if (n == 1) return 1;
 
-    if (cache[n]) {
-        return cache[n];
-    }
-    else {
-        if (n % 2 == 1) {
-            return cache[n] = colatz(partial_result + 1, 3 * n + 1);
+    if (n < 100000000) {
+        if (cache[n]) {
+            return cache[n];
         }
         else {
-            return cache[n] = colatz(partial_result + 1, n / 2);
+            if (n % 2 == 1) {
+                return cache[n] = 1 + colatz( 3 * n + 1);
+            }
+            else {
+                return cache[n] = 1 + colatz(n / 2);
+            }
+        }
+    }
+    else {
+         if (n % 2 == 1) {
+            return 1 + colatz(3 * n + 1);
+        }
+        else {
+            return 1 + colatz(n / 2);
         }
     }
 }
 
 int main() {
+    int a, b;
+    cin >> a >> b;
 
-    printf("%d\n", colatz(1, 60975));
+    int ans = INT8_MAX;
+    int cnt = INT8_MIN;
+    for (a; a <= b; a++) {
+        int tmp = colatz(a);
 
-    // long long int a, b;
-    // cin >> a >> b;
-
-    // int ans = INT8_MAX;
-    // int cnt = INT8_MIN;
-    // int tmp;
-    // for (a; a <= b; a++) {
-    //     tmp = colatz(1, a);
-
-    //     if (tmp > cnt) {
-    //         ans = a;
-    //         cnt = tmp;
-    //     }
-    //     else if (tmp == cnt && ans > a) {
-    //         ans = a;
-    //     }
-    // }
-    // cout << ans << " " << cnt << endl;
+        if (tmp > cnt) {
+            ans = a;
+            cnt = tmp;
+        }
+        else if (tmp == cnt && ans > a) {
+            ans = a;
+        }
+    }
+    cout << ans << " " << cnt << endl;
 
     return 0;
 }
