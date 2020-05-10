527273a99d93f0b89e72bd9086b78dae3f8a0043
gwkim <youlive789@gmail.com>
2020-05-10 18:18:42 +0900

    feat: Add linked list scratch

--- Cpp/DataStructure/LinkedList.h
+++ Cpp/DataStructure/LinkedList.h
@@ -1,3 +1,5 @@
+#pragma once
+
 class Node {
 public:
     int data;
@@ -6,11 +8,62 @@ public:
 };
 
 class LinkdedList {
+private:
+    Node * head = nullptr;
 public:
-    Node * head;
-    void insertNode(const Node* node) {}
-    void deleteNode(const int& idx) {}
-    void searchNode(const Node* node) {}
+
+    LinkdedList() {}
+
+    void insertNode(Node* node) {
+        // 처음 삽입 시
+        if (this->head == nullptr) {
+            this->head = node;
+            return;
+        }
+        // 그 외의 경우
+        Node* cursor = this->head;
+        while (cursor->next != nullptr) {
+            cursor = cursor->next;
+        }
+        cursor->next = node;
+    }
+
+    void deleteNode(const int& idx) {
+
+        if (idx == 0) {
+            this->head = this->head->next;
+            return;
+        }
+
+        int index = 0;
+        Node* cursor = this->head;
+        while (index != idx - 2) {
+            cursor = cursor->next;
+            index++;
+        }
+
+        Node* nextNode = cursor->next->next;
+        cursor->next = nextNode;
+    }
+
+    int getNode(const int& idx) {
+        int index = 0;
+        Node* cursor = this->head;
+        while (index != idx) {
+            cursor = cursor->next;
+            index++;
+        }
+
+        return cursor->data;
+    }
+
+    void printList() {
+        Node * cursor = this->head;
+        while (cursor != nullptr) {
+            std::cout << cursor->data << std::endl;
+            cursor = cursor->next;
+        }
+    }
 };
 
 
--- Cpp/DataStructure/main.cpp
+++ Cpp/DataStructure/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "LinkedList.h"
 
 int main() {
+    
+    LinkdedList* list = new LinkdedList();
+    for (int i = 0; i < 10; i++) {
+        list->insertNode(new Node(i));
+    }
+
+    list->deleteNode(5);
+    list->printList();
+
+    std::cout << "get : " << list->getNode(5) << std::endl;
+
+    delete[] list;
+
     return 0;
 }
