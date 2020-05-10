aff8aee373635893530d8faf5fbef609271fd764
gwkim <youlive789@gmail.com>
2020-05-10 17:05:15 +0900

    feat: Add datastructure scratch

---
+++ Cpp/DataStructure/LinkedList.h
@@ -0,0 +1,16 @@
+class Node {
+public:
+    int data;
+    Node * next;
+    Node(int data) : data(data) {}
+};
+
+class LinkdedList {
+public:
+    Node * head;
+    void insertNode(const Node* node) {}
+    void deleteNode(const int& idx) {}
+    void searchNode(const Node* node) {}
+};
+
+
