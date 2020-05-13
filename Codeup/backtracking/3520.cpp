e61ef2bf7e701218ca74d2d0cf226289da4f0586
gwkim <youlive789@gmail.com>
2020-05-13 12:17:06 +0900

    feat: Modify n queen problem

--- Codeup/backtracking/3520.cpp
+++ Codeup/backtracking/3520.cpp
@@ -9,30 +9,26 @@ bool chess[13][13];
 void flip(bool can, int col, int row, int N) {
     for (int r = 0; r < N; r++) {
         for (int c = 0; c < N; c++) {
-            // 행
             if (r == row) chess[r][c] = can;
-            // 열
             if (c == col) chess[r][c] = can;
-            // 대각선
             if (abs(col - row) == abs(c - r)) chess[r][c] = can;
             if (abs(col + row) == abs(c + r)) chess[r][c] = can;
         }
     }
 }
 
-void queen(int col, int N, int cntQueen) {
+void queen(int col, int N) {
 
     // 기저사례: 모든 열을 다 확인한 경우
     if (col == N) {
-        // 퀸을 모두 배치할 수 있었다면
-        if (N == cntQueen) answer++;
+        answer++;
         return;
     }
 
     for (int i = 0; i < N; i++) {
         if (chess[i][col]) {
-            flip(false, col, i, N);
-            queen(col+1, N, cntQueen+1);
+            flip(false, col, i, N);          
+            queen(col+1, N);
             flip(true, col, i, N);
         }
     }
@@ -49,7 +45,7 @@ int main() {
         }
     }
 
-    queen(0, N, 0);
+    queen(0, N);
     cout << answer << endl;
 
     return 0;
