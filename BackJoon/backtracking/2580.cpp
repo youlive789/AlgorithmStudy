a8d9f602639f73a0fc54eb12b82679d2e58c833b
gwkim <youlive789@gmail.com>
2020-05-18 16:30:10 +0900

    feat: Modify backjoon 2580

--- BackJoon/backtracking/2580.cpp
+++ BackJoon/backtracking/2580.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
 #include <set>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 
+int col[9];
+int row[9];
+int sqaure[9];
 int grid[9][9];
 
 // 둘 수 있는 최소 숫자를 가져온다.
 int getLatestNumber(const set<int>& numbers) {
-    int last = 0, result = 0;
-    for (int num : numbers) {
-        if (last == 0) {
-            last = num;
-            continue;
+    int result = 0;
+    vector<int> tmp;
+    set<int> big = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    set_difference(big.begin(), big.end(), numbers.begin(), numbers.end(), back_inserter(tmp));
+    
+    for (int i = 1; i <= 9; i++) {
+        if (find(tmp.begin(), tmp.end(), i) != tmp.end()) {
+            result = i;
+            break;
         }
-        if (num - last > 1) result = num + 1;
     }
     return result;
 }
@@ -67,10 +75,6 @@ pair<int, int> putSquare(int idx) {
     return make_pair(firstZeroRow, firstZeroCol);
 }
 
-void rollback(pair<int,int> p) {
-    grid[p.first][p.second] = 0;
-}
-
 // 그리드가 가득 찼는지 검사한다.
 bool done() {
     for (int r = 0; r < 9; r++) {
@@ -81,24 +85,62 @@ bool done() {
     return true;
 }
 
-void sudoku() {
-    if (done()) {
-        return;
+// 0 개수 업데이트
+void update() {
+    memset(row, 0, sizeof(row));
+    memset(col, 0, sizeof(col));
+    memset(sqaure, 0, sizeof(sqaure));
+
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            if (grid[r][c] == 0) {
+                row[r]++;
+                col[c]++;
+            }
+        }
     }
+
+    int idx = 0, cnt = 0;
+    for (int r = 0; r < 9; r += 3) {
+        for (int c = 0; c < 9; c += 3) {
+            if (grid[r][c] == 0) cnt++;
+            if (grid[r][c+1] == 0) cnt++;
+            if (grid[r][c+2] == 0) cnt++;
+            if (grid[r+1][c] == 0) cnt++;
+            if (grid[r+1][c+1] == 0) cnt++;
+            if (grid[r+1][c+2] == 0) cnt++;
+            if (grid[r+2][c] == 0) cnt++;
+            if (grid[r+2][c+1] == 0) cnt++;
+            if (grid[r+2][c+2] == 0) cnt++;
+            sqaure[idx] = cnt;
+            cnt = 0;
+            idx++;
+        }
+    }
+}
+
+void sudoku() { 
+    // 중복된 값이 입력됐을 때 리턴처리 필요
+    if (done()) return;
+
+    update();
 
     pair<int,int> p;
     for (int idx = 0; idx < 9; idx++) {
-        p = putRow(idx);
-        sudoku();
-        rollback(p);
+        if (row[idx] > 0) {
+            p = putRow(idx);
+            sudoku();
+        }
+        
+        if (col[idx] > 0) {
+            p = putCol(idx);
+            sudoku();
+        }
 
-        p = putCol(idx);
-        sudoku();
-        rollback(p);
-
-        p = putSquare(idx);
-        sudoku();
-        rollback(p);
+        if (sqaure[idx] > 0) {
+            p = putSquare(idx);
+            sudoku();
+        }
     }
 }
 
@@ -111,7 +153,7 @@ int main() {
             grid[r][c] = tmp;
         }
     }
-
+    update();
     sudoku();
     cout << endl;
     for (int r = 0; r < 9; r++) {
@@ -123,3 +165,17 @@ int main() {
 
     return 0;
 }
+
+/*
+
+0 3 5 4 6 9 2 7 8
+7 8 2 1 0 5 6 0 9
+0 6 0 2 7 8 1 3 5
+3 2 1 0 4 6 8 9 7
+8 0 4 9 1 3 5 0 6
+5 9 6 8 2 0 4 1 3
+9 1 7 6 5 2 0 8 0
+6 0 3 7 0 1 9 5 2
+2 5 8 3 9 4 7 6 0
+
+*/
