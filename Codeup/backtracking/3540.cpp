ae82205543db068a58b2c4d27d3207d2334116b1
gwkim <youlive789@gmail.com>
2020-05-21 17:20:19 +0900

    feat: Solved codeup 3540

--- Codeup/backtracking/3540.cpp
+++ Codeup/backtracking/3540.cpp
@@ -1,22 +1,75 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <deque>
 
 using namespace std;
 
 vector<char> opers;
-char oper[3] = {'+', '-', ' '};
+char oper[3] = {' ', '+', '-'};
 
 int calculate(int N) {
-    // queue를 이용해서 구현하자!
-    return 0;
+    vector<int> tmp;
+    vector<int> eraseIdx;
+    deque<int> numbers;
+
+    // 임시 숫자 배열생성
+    for (int i = 1; i <= N; i++) {
+        tmp.push_back(i);
+    }   
+    
+    // 공백연산 처리
+    int numIdx = 0;
+    for (int i = 0; i < opers.size(); i++) {
+        if (opers[i] == ' ') {
+            tmp[numIdx] = tmp[numIdx] * 10 + tmp[numIdx+1];
+            tmp.erase(tmp.begin() + (numIdx + 1));
+        }
+        else {
+            numIdx++;
+        }
+    }
+
+    // 실제 계산할 숫자 삽입
+    for (int num : tmp) {
+        numbers.push_back(num);
+    }
+    
+    // 연산 수행
+    for (char op : opers) {
+        if (op == ' ') {
+            continue;
+        }
+        else if (op == '+') {
+            int a = numbers.front();
+            numbers.pop_front();
+
+            int b = numbers.front();
+            numbers.pop_front();
+
+            int num = a + b;
+            numbers.push_front(num);
+        }
+        else if (op == '-') {
+            int a = numbers.front();
+            numbers.pop_front();
+
+            int b = numbers.front();
+            numbers.pop_front();
+
+            int num = a - b;
+            numbers.push_front(num);
+        }
+    }
+
+    return numbers.front();
 }
 
 void makeZero(int idx, int cntOper, int N) {
     if (idx == cntOper) {
         if (calculate(N) == 0) {
             for (int i = 0; i < opers.size(); i++) {
-                cout << (i+1) << " ";
-                cout << opers[i] << " ";
+                cout << (i+1);
+                cout << opers[i];
             }
             cout << N << endl;
         }
