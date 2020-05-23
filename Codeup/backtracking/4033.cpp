04df026380934fb2fdae767551d054a51b31b6e8
gwkim <youlive789@gmail.com>
2020-05-23 17:25:45 +0900

    feat: Solved codeup 4033

--- Codeup/backtracking/4033.cpp
+++ Codeup/backtracking/4033.cpp
@@ -19,42 +19,9 @@ int unfill(int startIdx, int endIdx) {
     }
 }
 
-bool validate(int N) {
-    vector<int> checker;
-
-    int blackCnt = 0;
-    bool blackTrigger = false;
-    for (int idx = 0; idx < N; idx++) {
-        if (grid[idx] == true) {
-            blackCnt++;
-            blackTrigger = true;
-        }
-        else {
-            if (blackTrigger) checker.push_back(blackCnt);
-            blackCnt = 0;
-            blackTrigger = false;
-        }
-
-        if (idx + 1 == N && blackTrigger) checker.push_back(blackCnt);
-    }
-
-    if (checker.size() != black.size()) return false;
-    
-    // for (int i = 0; i < checker.size(); i++) {
-    //     if (checker[i] != black[i]) return false;
-    // }
-
-    return true;
-}
-
-void dfs(int index, int N, int toChoose) {
-    // 기저사례: 더 이상 칠할 수 없다면
-    // for (int i = 0; i < N; i++) {
-    //     cout << grid[i] << " ";
-    // }
-    // cout << " ==> index : " << index << " toChoose : " << toChoose << endl;
-
-    if (validate(N)) {
+void dfs(int index, int N, int toChoose, int cnt) {
+    // 기저사례: K만큼 칠했다면
+    if (cnt == black.size()) {
         answer++;
         return;
     }
@@ -65,9 +32,9 @@ void dfs(int index, int N, int toChoose) {
 
         if (index + size - 1 < N) {
             fill(index, size);
-            dfs(index + size + 1, N, idx + 1);
+            dfs(index + size + 1, N, idx + 1, cnt + 1);
             unfill(index, index + size - 1);
-            dfs(index + 1, N, idx);
+            dfs(index + 1, N, idx, cnt);
         }
     }
 }
@@ -89,7 +56,7 @@ int main() {
         return 0;
     }
     else {
-        dfs(0, N, 0);
+        dfs(0, N, 0, 0);
         cout << answer << endl;
         return 0;
     }
