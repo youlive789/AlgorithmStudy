74c4d1fbfdd5efd7121be30af31b0b3b7f8fd9a8
gwkim <youlive789@gmail.com>
2020-05-30 19:15:52 +0900

    feat: Modify euler project 16

--- euler/first/16.cpp
+++ euler/first/16.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <limits>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main() {
 
+    // double test = 11;
+    // double tmp = floor(11/2);
+    // double rest = test - (tmp * 2);
+    // cout << tmp << " " << rest << endl;
+
+    
     double test = pow(2, 1000);
+    ostringstream numberStream;
+    numberStream << fixed << setprecision(numeric_limits<double>::digits10) << test;
+    string number = numberStream.str();
+
+    unsigned long long result = 0;
+
     cout << test << endl;
-    double result = 0;
-    while (test) {
-        double tmp = test / 10;
-        double rest = test - (tmp * 10);
-        result += rest;
-        test = tmp;
+
+    for (char num : number) {
+        result += (num - '0');
     }
 
     cout << result << endl;
