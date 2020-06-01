7ff2023115efa7fff1c1e94cd366ce0f1f92d2b1
gwkim <youlive789@gmail.com>
2020-06-02 08:51:34 +0900

    feat: Modify codeup 4637 to backtracking method

--- Codeup/backtracking/4637.cpp
+++ Codeup/backtracking/4637.cpp
@@ -9,95 +9,80 @@ struct round {
     int lose;
 };
 
-bool checkAllRounds(int scoreTable[18]) {
-    round rounds[6];
-    for (int idx = 0; idx < 6; idx++) {
-        rounds[idx].win = scoreTable[idx * 3];
-        rounds[idx].draw = scoreTable[idx * 3 + 1];
-        rounds[idx].lose = scoreTable[idx * 3 + 2];
+bool result[4];
+
+void cntRounds(int cases, round rounds[6], int nowIdx) {
+
+    if (nowIdx == 6) {
+        for (int i = 0; i < 6; i++) {
+            if (rounds[i].win != 0 || rounds[i].draw != 0 || rounds[i].lose != 0) break;
+        }
+        result[cases] = true;
+        return;
     }
 
-    for (int idx = 0; idx < 6; idx++) {
-
-        // 승수 체크
-    
-        // 무승부 체크
-
-        // 패수 체크
+    if (cases == 2) {
+        cout << "경기 인덱스 " << cases << " ==> ";
+        for (int i = 0; i < 6; i++) {
+            cout << rounds[i].win << " ";
+            cout << rounds[i].draw << " ";
+            cout << rounds[i].lose << " ";
+        }
+        cout << endl;
     }
 
-    return true;
+    // 모든 국가끼리 경기를 수행해본다.
+    for (int idx = nowIdx; idx < 5; idx++) {
+        // 승
+        if (rounds[idx].win > 0 && rounds[idx+1].lose > 0) {
+            rounds[idx].win--;
+            rounds[idx+1].lose--;
+            cntRounds(cases, rounds, idx+1);
+            rounds[idx].win++;
+            rounds[idx+1].lose++;
+        }
+
+        // 무승부
+        if (rounds[idx].draw > 0 && rounds[idx+1].draw > 0) {
+            rounds[idx].draw--;
+            rounds[idx+1].draw--;
+            cntRounds(cases, rounds, idx+1);
+            rounds[idx].draw++;
+            rounds[idx+1].draw++;
+        }
+
+        // 패
+        if (rounds[idx].lose > 0 && rounds[idx+1].win > 0) {
+            rounds[idx].lose--;
+            rounds[idx+1].win--;
+            cntRounds(cases, rounds, idx+1);
+            rounds[idx].lose++;
+            rounds[idx+1].win++;
+        }
+    }
 }
 
 int main() {
-    int scoreTable[4][18];
-    int cases = 4, casesIdx = 4;
-    while (cases--) {
-        for (int idx = 0; idx < 18; idx++) {
+
+    round rounds[4][6];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 18; j++) {
             int tmp;
             cin >> tmp;
-            scoreTable[4 - casesIdx][idx] = tmp;
-        }   
-        casesIdx--;
+
+            if (j % 3 == 0) rounds[i][j / 3].win = tmp;
+            if (j % 3 == 1) rounds[i][j / 3].draw = tmp;
+            if (j % 3 == 2) rounds[i][j / 3].lose = tmp;
+        }
     }
 
-    for (int cases = 0; cases < 4; cases++) {
-        int totalWin = 0, totalLoses = 0;
+    for (int i = 0; i < 4; i++)
+        cntRounds(i, rounds[i], 0);
 
-        // 승 계산
-        for (int idx = 0; idx < 18; idx += 3)
-            totalWin += scoreTable[cases][idx];
-
-        // 패 계산
-        for (int idx = 2; idx < 18; idx += 3)
-            totalLoses += scoreTable[cases][idx];
-        
-        //cout << "총승 : " << totalWin << " 총패 : " << totalLoses << endl;
-
-        // 승수와 패수가 다르다면 불가능한 결과
-        if (totalWin != totalLoses) {
-            cout << 0 << " ";
-            continue;
-        }
-
-        // 무승부는 짝을 이루어야한다.
-        int drawScore = 0, drawTrigger = 1;
-        for (int idx = 1; idx < 18; idx += 3) {
-            drawScore += (scoreTable[cases][idx] * drawTrigger);
-            drawTrigger *= -1;
-        }
-
-        //cout << "무승부 : " << drawScore << endl;
-
-        // 무승부가 짝을 이루지 않는다면
-        if (drawScore != 0) {
-            cout << 0 << " ";
-            continue;
-        }
-
-        // 총 경기수 계산
-        set<int> rounds;
-        int totalRounds;
-        for (int idx = 0; idx < 18; idx += 3) {
-            totalRounds = scoreTable[cases][idx] + scoreTable[cases][idx + 1] + scoreTable[cases][idx + 2];
-            rounds.insert(totalRounds);
-        }
-
-        // 총 경기수가 다르다면
-        if (rounds.size() > 1) {
-            cout << 0 << " ";
-            continue;
-        }
-
-        // 모든 국가와 경기했는지 확인
-        if (checkAllRounds(scoreTable[cases])) {
-            cout << 1 << " ";
-        }
-        else {
-            cout << 0 << " ";
-        }
-        
-    }
+    for (int i = 0; i < 4; i++)
+        cout << result[i] << " ";
+    cout << endl;
 
     return 0;
 }
@@ -114,11 +99,4 @@ int main() {
 5 0 0 0 0 5 4 0 1 1 0 4 5 0 0 0 0 5
 1 3 1 1 3 1 4 0 1 1 0 4 4 0 1 1 0 4
 => 1 0 0 0
-
-4 1 0 
-3 1 1 
-2 1 2 
-2 1 2 
-0 1 4 
-1 1 3
 */
