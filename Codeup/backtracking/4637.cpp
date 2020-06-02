4afa1c35587c41e1af463effb9ed0383dd03d105
gwkim <youlive789@gmail.com>
2020-06-02 15:53:09 +0900

    feat: Modify codeup 4637

--- Codeup/backtracking/4637.cpp
+++ Codeup/backtracking/4637.cpp
@@ -11,49 +11,27 @@ struct round {
 
 bool result[4];
 
-void cntRounds(int cases, round rounds[6], int firstIdx, int secondIdx, int cnt) {
+void cntRounds(int cases, round rounds[6]) {
 
-    if (cnt == 15) {
-        for (int i = 0; i < 6; i++) {
-            if (rounds[i].win != 0 || rounds[i].draw != 0 || rounds[i].lose != 0) return;
-        }
-        result[cases] = true;
-        return;
-    }
 
     // 모든 국가끼리 경기를 수행해본다.
-    int lastTrigger = 0;
-    for (int i = firstIdx; i < 5; i++) {
-        for (int j = secondIdx; j < 6; j++) {
-            // 승
-            if (rounds[i].win > 0 && rounds[j].lose > 0) {
-                rounds[i].win--;
-                rounds[j].lose--;
-                cntRounds(cases, rounds, i, j, cnt+1);
-                rounds[i].win++;
-                rounds[j].lose++;
-            }
-
-            // 무승부
-            if (rounds[i].draw > 0 && rounds[j].draw > 0) {
-                rounds[i].draw--;
-                rounds[j].draw--;
-                cntRounds(cases, rounds, i, j, cnt+1);
-                rounds[i].draw++;
-                rounds[j].draw++;
-            }
-
-            // 패
-            if (rounds[i].lose > 0 && rounds[j].win > 0) {
-                rounds[i].lose--;
-                rounds[j].win--;
-                cntRounds(cases, rounds, i, j, cnt+1);
-                rounds[i].lose++;
-                rounds[j].win++;
-            }
-        }       
-    }
+    
 }
+
+/*
+4 0 0 4 0 1 3 0 2 2 0 3 1 0 4 0 0 4
+5 0 0 0 5 0 0 0 5 5 0 0 0 5 0 0 0 5
+2 3 0 0 0 5 2 3 0 2 3 0 1 0 4 2 3 0
+4 1 0 3 1 1 2 1 2 2 1 2 0 1 4 1 1 3
+=> 0 0 1 1
+
+0 5 0 0 5 0 0 5 0 0 5 0 0 5 0 0 5 0
+1 4 0 1 4 0 0 4 1 0 4 1 3 0 2 2 0 3
+5 0 0 0 0 5 4 0 1 1 0 4 5 0 0 0 0 5
+1 3 1 1 3 1 4 0 1 1 0 4 4 0 1 1 0 4
+=> 1 0 0 0
+*/
+
 
 int main() {
 
@@ -79,24 +57,3 @@ int main() {
 
     return 0;
 }
-
-/*
-4 0 0 4 0 1 3 0 2 2 0 3 1 0 4 0 0 4
-5 0 0 0 5 0 0 0 5 5 0 0 0 5 0 0 0 5
-2 3 0 0 0 5 2 3 0 2 3 0 1 0 4 2 3 0
-4 1 0 3 1 1 2 1 2 2 1 2 0 1 4 1 1 3
-=> 0 0 1 1
-
-0 5 0 0 5 0 0 5 0 0 5 0 0 5 0 0 5 0
-1 4 0 1 4 0 0 4 1 0 4 1 3 0 2 2 0 3
-5 0 0 0 0 5 4 0 1 1 0 4 5 0 0 0 0 5
-1 3 1 1 3 1 4 0 1 1 0 4 4 0 1 1 0 4
-=> 1 0 0 0
-
-4 0 0 
-4 0 1 
-3 0 2 
-2 0 3 
-1 0 4 
-0 0 4
-*/
