ec236e8b5f1c486e818eb8134b0d5aea841369cb
gwkim <youlive789@gmail.com>
2020-06-04 11:35:07 +0900

    feat: Solved euler problem 16

--- euler/first/16.cpp
+++ euler/first/16.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
 int main() {
+
+    vector<int> a;
+    a.push_back(2);
+
+    int cases = 999;
+    while (cases--) {
+        for (int idx = 0; idx < a.size(); idx++) a[idx] *= 2;
+        for (int idx = 0; idx <= a.size() - 1; idx++) {
+            if (a[idx] >= 10) {
+                if (idx == a.size() - 1) {
+                    int tmp = a[idx] / 10;
+                    a.push_back(tmp);
+                    a[idx] %= 10;
+                }
+                else {
+                    int tmp = a[idx] / 10;
+                    a[idx+1] += tmp;
+                    a[idx] %= 10;
+                }
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int num : a) {
+        sum += num;
+    }
+
+    cout << sum << endl;
 
     return 0;
 }
