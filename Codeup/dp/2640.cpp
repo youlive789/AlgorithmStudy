3ea23141d36631fcd37b5602c8224f6d3879638d
gwkim <youlive789@gmail.com>
2020-06-05 11:32:15 +0900

    feat: Modify codeup 2640

--- Codeup/dp/2640.cpp
+++ Codeup/dp/2640.cpp
@@ -3,40 +3,22 @@
 
 using namespace std;
 
-vector<int> target;
+int cache[100000003];
 
+int mul(int n, int k) {
+    if (k == 1) {
+        return n;
+    }
+    else {
+        return n * mul(n, k-1);
+    }
+}
 
 int main() {
 
     int n, k;
     cin >> n >> k;
-    k--;
-    target.push_back(n);
-    while (k--) {
-        for (int idx = 0; idx < target.size(); idx++) {
-            target[idx] *= n;
-        }
-            
-        for (int idx = 0; idx < target.size(); idx++) {
-            if (target[idx] >= 10) {
-                if (idx == target.size() - 1) {
-                    int tmp = target[idx] / 10;
-                    target.push_back(tmp);
-                    target[idx] %= 10;
-                }
-                else {
-                    int tmp = target[idx] / 10;
-                    target[idx+1] += tmp;
-                    target[idx] %= 10;
-                }
-            }
-        }
-    }
 
-    for (int idx = target.size() - 1; idx >= 0; idx--) {
-        cout << target[idx];
-    }
-    cout << endl;
 
     return 0;
 }
