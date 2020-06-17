ae3821e1b7ed0132aab5a18fd44418c4b2a44394
gwkim <youlive789@gmail.com>
2020-06-18 08:58:33 +0900

    feat: Solved euler project 25

--- euler/second/25.cpp
+++ euler/second/25.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-unsigned long long fibo(int n) {
-
-    if (n == 1) {
-        return 1;
-    }
-    else if (n == 2) {
-        return 1;
-    }
-    else {
-        return fibo(n-1) + fibo(n-2);
-    }
-
-}
-
 int main() {
+    vector<int> fibo;
+    fibo.push_back(1);
 
+    vector<int> num;
+    num.push_back(1);
+
+    vector<int> tmp;
+
+    unsigned int idx = 2;
+    while (true) {
+
+        tmp = fibo;
+
+        for (int i = 0; i < num.size(); i++) {
+            fibo[i] += num[i];
+        }
+
+        for (int i = 0; i < fibo.size(); i++) {
+            if (fibo[i] >= 10) {
+                int carry = fibo[i] / 10;
+                fibo[i] %= 10;
+
+                if (i == fibo.size() - 1) {
+                    fibo.push_back(carry);
+                }
+                else {
+                    fibo[i+1] += carry;
+                }
+            }
+        }
+        idx++;
+
+        if (fibo.size() >= 1000) break;
+
+        num = tmp;
+    }
+
+    cout << "index : " << idx << endl;
+    for (int num : fibo) cout << num;
+    
+    cout << endl;
 }
 
 
