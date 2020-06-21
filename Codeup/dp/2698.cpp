d500ca2958b55c212d8ddadccc4694dc8ac14d17
gwkim <youlive789@gmail.com>
2020-06-22 08:40:43 +0900

    feat: Solved codeup 2698

--- Codeup/dp/2698.cpp
+++ Codeup/dp/2698.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
+int result = 0;
+
 void move(int N, char start, char to) {
-    
+    printf("%d : %c->%c\n", N, start, to);
+    result++;
 }
 
 void hanoi(int N, char start, char via, char to) {
-
+    if (N == 1) {
+        move(N, start, via);
+        move(N, via, to);
+    }
+    else {
+        hanoi(N-1, start, via, to);
+        move(N, start, via);
+        hanoi(N-1, to, via, start);
+        move(N, via, to);
+        hanoi(N-1, start, via, to);
+    }
 }
 
 int main() {
-
+    int N;
+    cin >> N;
+    hanoi(N, 'A', 'B', 'C');
+    cout << result << endl;
     return 0;
 }
-
-/*
-실제 이동과정 풀이
-
-*/
