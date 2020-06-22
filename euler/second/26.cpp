79e372a4a4beccf99b9592b499f0c7a9f17872a1
gwkim <youlive789@gmail.com>
2020-06-22 12:40:46 +0900

    feat: Modify euler 26

--- euler/second/26.cpp
+++ euler/second/26.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    vector<int> numlist;
+
+    int q = 10, r = 0, t = 3;
+    for (int t = 1; t < 10; t ++) {
+        numlist.push_back(q / t);
+        q = q % t * 10;
+    }
+    
+    for (auto t : numlist) {
+        cout << t;
+    }
+    cout << endl;
+
     return 0;
 }
