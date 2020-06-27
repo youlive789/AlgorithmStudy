bc5b977a71108905133a3b3ae5336ede5b61c27d
gwkim <youlive789@gmail.com>
2020-06-27 17:52:38 +0900

    feat: Solved euler project 28

--- euler/second/28.cpp
+++ euler/second/28.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-#define SIZE 5
-#define SIZE_T 4
+#define SIZE 1001
+#define SIZE_T 1000
 
 
 int table[SIZE][SIZE];
 
 int main() {
 
-    int origin = (SIZE * SIZE) - SIZE_T;
+    int origin = (SIZE * SIZE);
     int row = 0, col = SIZE_T;
     int trigger = 0; // 0: 좌측, 1: 하단, 2: 우측, 3:상단
 
-    int answer = 0;
+    long long unsigned int answer = 0;
 
-    while (true) {
+    int step = SIZE * SIZE;
+
+    while (step--) {
        
-        cout << row << " " << col << endl;
-
+        if (abs(row - col) == 0 || abs(row + col) == SIZE_T) answer += origin;
 
         table[row][col] = origin;
-        origin++;
+        origin--;
 
         // 이동수행
         // 좌측으로 이동중이였다면
@@ -42,54 +44,39 @@ int main() {
             row--;
         }
 
-        if (row == 500 && col == 500) break;
-
         // 방향설정
         // 우측으로 이동중이였다면
         if (trigger == 0) {
             // 경계조건
-            if (col >= 0) {
-                if (col == 0 || table[row][col] > 0) {
-                    trigger = 1;
-                }
+            if (col == 0 || table[row][col-1] > 0) {
+                trigger = 1;
             }
+            
         }
         // 하단으로 이동중이였다면
         else if (trigger == 1) {
             // 경계조건
-            if (row <= SIZE_T) {
-                if (row == SIZE_T || table[row][col] > 0) {
-                    trigger = 2;
-                }
+            if (row == SIZE_T || table[row+1][col] > 0) {
+                trigger = 2;
             }
         }
         // 좌측으로 이동중이였다면
         else if (trigger == 2) {
             // 경계조건
-            if (col <= SIZE_T) {
-                if (col == SIZE_T || table[row][col] > 0) {
-                    trigger = 3;
-                }
+            if (col == SIZE_T || table[row][col+1] > 0) {
+                trigger = 3;
             }
         }
         // 상단으로 이동중이였다면
         else if (trigger == 3) {
             // 경계조건
-            if (row >= 0) {
-                if (row == 0 || table[row][col] > 0) {
-                    trigger = 0;
-                }
+            if (row == 0 || table[row-1][col] > 0) {
+                trigger = 0;
             }
         }
-
-
     }
 
-    // for (int row = 0; row < 1001; row++)
-    //     for (int col = 0; col < 1001; col++)
-    //         cout << table[row][col] << " ";
-    //     cout << endl;
-
+    cout << answer << endl;
 
     return 0;
 }
