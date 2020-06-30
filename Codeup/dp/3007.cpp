802598c984f56b4664cfc136923388ee0bc5fc33
gwkim <youlive789@gmail.com>
2020-06-30 13:53:06 +0900

    feat: Modify codeup 3007

--- Codeup/dp/3007.cpp
+++ Codeup/dp/3007.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
-#include <algorithm>
+#include <map>
+#include <string>
 
 using namespace std;
 
 vector<int> numbers;
+map<string, int> cache;
+
+int sums(int start, int end) {
+    if (start == end) {
+        return numbers[start];
+    }
+    else {
+        string nowIndex = to_string(start) + to_string(end);
+        if (cache[nowIndex]) {
+            return cache[nowIndex];
+        }
+        else {
+            return cache[nowIndex] = numbers[start] + sums(start + 1, end);
+        }
+    }
+}
 
 int main() {
 
@@ -24,7 +41,11 @@ int main() {
 
         startIdx--;
         endIdx--;
+
+        cout << sums(startIdx, endIdx) << endl;
     }
-    
+
+    // cout << sums(0, 2) << endl;
+
     return 0;
 }
