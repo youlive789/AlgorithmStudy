a8fe91e0e0ee01a3167d5f47297c3d21043660b0
gwkim <youlive789@gmail.com>
2020-06-30 09:02:09 +0900

    feat: Modify codeup 3007

--- Codeup/dp/3007.cpp
+++ Codeup/dp/3007.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> numbers;
+vector<int> idx_start;
+vector<int> idx_end;
 
 int main() {
 
@@ -20,8 +23,48 @@ int main() {
     while (cases--) {
         int startIdx, endIdx;
         cin >> startIdx >> endIdx;
+
+        startIdx--;
+        endIdx--;
+
+        idx_start.push_back(startIdx);
+        idx_end.push_back(endIdx);
     }
     
+    // 가장 큰 범위를 지정
+    int lowStart, maxEnd;
+    lowStart = *min_element(idx_start.begin(), idx_start.end());
+    maxEnd = *max_element(idx_end.begin(), idx_end.end());
+
+    // 합계를 미리 계산
+    int sumCache = 0;
+    for (int i = lowStart; i <= maxEnd; i++)
+        sumCache += numbers[i];
+
+    for (int i = 0; i < idx_start.size(); i++) {
+
+        int nowAnswer = sumCache;
+
+        if (idx_start[i] == idx_end[i]) {
+            cout << numbers[idx_start[i]] << endl;
+            continue;
+        }
+
+        if (lowStart < idx_start[i]) {
+            for (int idx = lowStart; idx < idx_start[i]; idx++) {
+                nowAnswer -= numbers[idx];
+            }
+                
+        }
+
+        if (maxEnd > idx_end[i]) {
+            for (int idx = idx_end[i] + 1; idx <= maxEnd; idx++) {
+                nowAnswer -= numbers[idx];
+            }
+        }
+
+        cout << nowAnswer << endl;
+    }
 
     return 0;
 }
