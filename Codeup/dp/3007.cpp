9bba57a86f552e7289bdbee15a10735e0729761c
gwkim <youlive789@gmail.com>
2020-07-01 12:09:11 +0900

    feat: Solved Codeup 3007 => cin,cout problem

--- Codeup/dp/3007.cpp
+++ Codeup/dp/3007.cpp
@@ -1,50 +1,30 @@
 #include <iostream>
 #include <cstdio>
-#include <vector>
-#include <map>
-#include <set>
 
 using namespace std;
-
-int * numbers;
-map<string, int> cache;
-
-int sums(int start, int end) {
-    if (start == end) {
-        return numbers[start];
-    }
-    else {
-        string nowIndex = to_string(start) + to_string(end);
-        if (cache[nowIndex]) {
-            return cache[nowIndex];
-        }
-        else {
-            return cache[nowIndex] = numbers[start] + sums(start + 1, end);
-        }
-    }
-}
 
 int main() {
 
     int numberCnt, cases;
     cin >> numberCnt >> cases;
 
-    numbers = new int[numberCnt];
+    int* sumTable = new int[numberCnt];
 
     for (int i = 0; i < numberCnt; i++) {
         int tmp;
-        cin >> tmp;
-        numbers[i] = tmp;
+        scanf("%d", &tmp);
+        if (i == 0) {
+            sumTable[i] = tmp;
+        }
+        else {
+            sumTable[i] = sumTable[i-1] + tmp;
+        }
     }
 
     while (cases--) {
         int startIdx, endIdx;
-        cin >> startIdx >> endIdx;
-
-        startIdx--;
-        endIdx--;
-
-        cout << sums(startIdx, endIdx) << endl;
+        scanf("%d %d", &startIdx, &endIdx);
+        printf("%d\n", sumTable[endIdx-1] - sumTable[startIdx-2]);
     }
 
     return 0;
