f0a18057efabe90a454a9be919685a51c4ea776e
gwkim <youlive789@gmail.com>
2020-07-02 12:12:46 +0900

    feat: Modify codeup 3703

--- Codeup/dp/3703.cpp
+++ Codeup/dp/3703.cpp
@@ -6,33 +6,34 @@ using namespace std;
 int grid[100][100];
 int bottomCache[100][100];
 int rightCache[100][100];
-int leftCache[100][100];
-int topCache[100][100];
 
 int findCandy(int y, int x, int N, int M) {
     if (y == N-1 && x == M-1) {
         return grid[y][x];
     }
 
-    int bottom = 0, right = 0, left = 0, top = 0;
-    printf("%d %d\n", x, y);
+    int bottom, right;
 
-    // bottom
-    if (y < N-1) bottom = grid[y][x] + findCandy(y + 1, x, N, M);
+    if (y < N-1) {
+        if (bottomCache[y][x]) {
+            return bottomCache[y][x];
+        }
+        else {
+            bottomCache[y][x] = bottom = grid[y][x] + findCandy(y + 1, x, N, M);
+        }
+        
+    }
 
-    // right
-    if (x < M-1) right = grid[y][x] + findCandy(y, x + 1, N, M);    
+    if (x < M-1) {
+        if (rightCache[y][x]) {
+            return rightCache[y][x];
+        }
+        else {
+            rightCache[y][x] = right = grid[y][x] + findCandy(y, x + 1, N, M);    
+        }
+    }
 
-    // top
-    if (y > 1) top = grid[y][x] + findCandy(y - 1, x, N, M);
-
-    // left
-    if (x > 1) left = grid[y][x] + findCandy(y, x - 1, N, M);    
-
-    int res = max(max(bottom, right), max(top, left));
-    printf("%d\n", res);
-
-    return res;
+    return max(bottom, right);
 }
 
 int main() {
