b285e68fee5429a719f4db1e3197df4a3794e074
gwkim <youlive789@gmail.com>
2020-07-06 08:46:43 +0900

    feat: Add codeup dp 3705

--- Codeup/dp/3705.cpp
+++ Codeup/dp/3705.cpp
@@ -1,22 +1,40 @@
 #include <cstdio>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
-int sumTable[100000];
+int numbers[100000];
+int cache[100000];
+
+int dfs(int nowIndex, int countNumber) {
+    if (nowIndex == countNumber) {
+        return numbers[countNumber-1];
+    }
+
+    int currentSum = 0, currentAnswer = numeric_limits<int>::min();
+    for (int i = nowIndex; i < countNumber; i++) {
+        currentSum += numbers[i];
+        currentAnswer = max(currentAnswer, currentSum);
+    }
+
+    return max(currentAnswer, dfs(nowIndex + 1, countNumber));
+}
 
 int main() {
 
     int cases;
-    scanf("%d", cases);
+    scanf("%d", &cases);
 
     int sum = 0;
     for (int i = 0; i < cases; i++) {
         int tmp;
         scanf("%d", &tmp);
         
-        sum += tmp;
-        sumTable[i] = sum;
+        numbers[i] = tmp;
     }
+
+    printf("%d", dfs(0, cases));
 
     return 0;
 }
