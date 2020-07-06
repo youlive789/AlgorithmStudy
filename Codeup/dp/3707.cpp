60762239658ed9604915bfc165d7c7170533106d
gwkim <youlive789@gmail.com>
2020-07-07 08:45:46 +0900

    feat: Solved codeup 3707

--- Codeup/dp/3707.cpp
+++ Codeup/dp/3707.cpp
@@ -2,25 +2,30 @@
 
 using namespace std;
 
-int cache[100000];
+unsigned long cache[100000];
 
-int dfs(int number) {
+unsigned long dfs(short number) {
 
     if (number == 0) {
         return 1;
     }
 
-    int sum = 0;
-    for (int idx = 1; idx <= number; idx++) {
-        sum += dfs(number - idx);
+    if (cache[number]) {
+        return cache[number];
     }
+    else {
+        unsigned long sum = 0;
+        for (int idx = 1; idx <= number; idx++) {
+            sum += cache[number - idx] = dfs(number - idx);
+        }
 
-    return sum;
+        return sum;
+    }
 }
 
 int main() {
 
-    int number;
+    short number;
     cin >> number;
 
     cout << dfs(number) - 1 << endl;
