55911c5272c8bbc769376deabf57d91ddcde9d65
gwkim <youlive789@gmail.com>
2020-07-09 08:53:22 +0900

    feat: Solved euler project 29

--- euler/second/29.cpp
+++ euler/second/29.cpp
@@ -1,31 +1,32 @@
 #include <set>
+#include <vector>
 #include <string>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 
 using namespace std;
 
 string powStr(const int& a, const int& b) {
-    string target = "";
-    target.append(to_string(a));
+    vector<int> target;
+    target.push_back(a);
 
     for (int idx = 1; idx < b; idx++) {
         
         // 첫째 자리에서 곱셈연산
         for (int i = 0; i < target.size(); i++) {
-            int nowNum = target[i] - '0';
-            nowNum *= a;
-            target[i] = nowNum + '0';
+            target[i] *= a;
         }
 
         // 자리수 연산
         for (int i = 0; i < target.size(); i++) {
-            int num = target[i] - '0';
+            int num = target[i];
             if (num >= 10) {
                 int carriage = num / 10;
                 num %= 10;
-                target[i] = num + '0';
+                target[i] = num;
                 if (i == target.size() - 1) {
-                    target.append(to_string(carriage));
+                    target.push_back(carriage);
                 }
                 else {
                     target[i+1] += carriage;
@@ -34,7 +35,11 @@ string powStr(const int& a, const int& b) {
         }
     }
 
-    return target;
+    ostringstream oss;
+    copy(target.begin(), target.end()-1, ostream_iterator<int>(oss, ","));
+    oss << target.back();
+
+    return oss.str();
 }
 
 int comb(const int& number) {
@@ -43,7 +48,8 @@ int comb(const int& number) {
 
     for (int i = 2; i <= number; i++) {
         for (int j = 2; j <= number; j++) {
-            numberSets.insert(powStr(i, j));
+            string t  = powStr(i, j);
+            numberSets.insert(t);
         }
     }
 
