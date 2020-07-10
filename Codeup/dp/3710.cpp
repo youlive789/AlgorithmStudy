893f83385cea635bfde599b463395293552f0cc9
gwkim <youlive789@gmail.com>
2020-07-10 18:53:10 +0900

    feat: Modify codeup 3710

--- Codeup/dp/3710.cpp
+++ Codeup/dp/3710.cpp
@@ -8,24 +8,33 @@ int getClab(const int& number) {
     int clab = 0;
     int target = number;
     while (target) {
-        if (cache[target] && cache[target] != number) {
+        if (cache[target]) {
             clab += cache[target];
             break;
         }
         else {
-            if (target % 10 == 3 || target % 10 == 6 || target % 10 == 9) clab++;
+            int tmp = target % 10;
+            if (tmp != 0 && tmp % 3 == 0) clab++;
             target /= 10;
         }
     }
     return cache[target] = clab;
 }
 
-int game(const int& a, const int& b) {
-    int answer = 0;
-    for (int idx = a; idx <= b; idx++) {
-        answer += getClab(idx);
+int conquer(const int& a, const int& b) {
+    if (a == b) {
+        if (cache[a]) {
+            return cache[a];
+        }
+        else {
+            return cache[a] = getClab(a);
+        }
     }
-    return answer;
+    else {
+        int left = conquer(a, (a + b) / 2);
+        int right = conquer(((a + b) / 2) + 1, b);
+        return left + right;
+    }
 }
 
 int main() {
@@ -33,7 +42,8 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    cout << game(a, b) << endl;
+    // cout << game(a, b) << endl;
+    cout << conquer(a, b) << endl;
 
     return 0;
 }
