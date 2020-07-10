e27d692ca1518235efce9290337442278f4b9a48
gwkim <youlive789@gmail.com>
2020-07-10 14:24:42 +0900

    feat: Modify codeup 3710

--- Codeup/dp/3710.cpp
+++ Codeup/dp/3710.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+// 에라토스 테네스채 방식으로 캐싱할 수 있는
+// 방법을 생각해보자.
 
+bool checkClab() {
+
+}
+
+int game(const int& a, const int& b) {
+
+}
 
 int main() {
+
+    int a, b;
+    cin >> a >> b;
+
     return 0;
 }
