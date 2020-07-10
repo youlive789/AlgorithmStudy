fdc23a5e0f4189dcebde43653b0152fdf6e9702a
gwkim <youlive789@gmail.com>
2020-07-10 18:30:22 +0900

    feat: Modify codeup 3710

--- Codeup/dp/3710.cpp
+++ Codeup/dp/3710.cpp
@@ -2,19 +2,38 @@
 
 using namespace std;
 
-// 에라토스 테네스채 방식으로 캐싱할 수 있는
-// 방법을 생각해보자.
+int cache[100000003];
+
+int getClab(const int& number) {
+    int clab = 0;
+    int target = number;
+    while (target) {
+        if (cache[target] && cache[target] != number) {
+            clab += cache[target];
+            break;
+        }
+        else {
+            if (target % 10 == 3 || target % 10 == 6 || target % 10 == 9) clab++;
+            target /= 10;
+        }
+    }
+    return cache[target] = clab;
+}
+
+int game(const int& a, const int& b) {
+    int answer = 0;
+    for (int idx = a; idx <= b; idx++) {
+        answer += getClab(idx);
+    }
+    return answer;
+}
 
 int main() {
 
     int a, b;
     cin >> a >> b;
 
+    cout << game(a, b) << endl;
+
     return 0;
 }
-
-/**
- * 
- * 
- * 
- */
