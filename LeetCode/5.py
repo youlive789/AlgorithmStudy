a3a1de3da903124f303799ba87734bbbdad53800
Geon-Woo Kim <youlive789@gmail.com>
2020-07-22 17:34:00 +0900

    feat: Add leetCode problem 5

---
+++ LeetCode/5.py
@@ -0,0 +1,25 @@
+target = "cbbd"
+
+
+answer = ""
+max_length = 0
+for i in range(len(target)):
+    for j in range(len(target) + 1):
+        if i < j:
+            s = target[i:j]
+            is_palindrome = False
+            mid = int(len(s) / 2)
+            print(s, " ", mid)
+            if len(s) % 2 == 0:
+                if s[:mid] == s[mid:][::-1]:
+                    is_palindrome = True
+            else:
+                if s[:mid] == s[mid+1:][::-1]:
+                    is_palindrome = True
+
+            if is_palindrome :
+                if max_length < len(s):
+                    max_length = len(s)
+                    answer = s
+
+print(answer)
