a4cc06aa20fc9eebb4e71ddd58f3c551aeff9f90
Geon-Woo Kim <youlive789@gmail.com>
2020-07-23 12:30:21 +0900

    feat: Fail leetCode problem 5

--- LeetCode/5.py
+++ LeetCode/5.py
@@ -6,35 +6,39 @@ class Solution:
         self.cache = [[0] * (len(s)+1)] * (len(s)+1)
         return self.answer(s, 0, len(s))
 
-    def answer(self, target, start, end) ->str:
-        # print(start, " ", end, " ", self.cache[start][end], " ", target[start:end])
+    def answer(self, target, start, end) -> str:
+        if target == '':
+            return target
+
         if self.cache[start][end] != 0:
             return self.cache[start][end]
         else:
             if len(target) == 1:
-                self.cache[start][end] = target
                 return target
             else:
                 if self.is_palindrome(target):
                     self.cache[start][end] = target
                     return target
 
-                if self.cache[start][end-1] != 0:
-                    left = self.cache[start][end-1]
-                else:
-                    left = self.cache[start][end-1] = self.answer(target[start:end-1], start, end - 1)
+                left = self.answer(target[start:end-1], start, end - 1)
+                mid = self.answer(target[start-1:end-1], start - 1, end - 1)
+                right = self.answer(target[start+1:end], start + 1, end)
 
-                if self.cache[start+1][end] != 0:
-                    right = self.cache[start+1][end]
-                else:
-                    right = self.cache[start+1][end] = self.answer(target[start+1:end], start + 1, end)
-
-                print(left, " ", right , " ==> ", target[start:end-1], " ", target[start+1:end])
-
-                if len(left) > len(right):
+                subs = [len(left), len(mid), len(right)]
+                idx = subs.index(max(subs))
+                if idx == 0:
+                    if left != '':
+                        self.cache[start][end] = left
                     return left
+                elif idx == 1:
+                    if mid != '':
+                        self.cache[start][end] = mid
+                    return mid
                 else:
+                    if right != '':
+                        self.cache[start][end] = right
                     return right
+
 
     def is_palindrome(self, target):
         mid = int(len(target) / 2)
@@ -49,6 +53,7 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
 
-    # print(s.longestPalindrome("babcb"))
-    print(s.longestPalindrome("eabcb"))
-    # print(s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
+    print(s.longestPalindrome("babcb"))
+    print(s.longestPalindrome("abcdbbfcba")) # bb
+    # print(s.cache)
+    print(s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
