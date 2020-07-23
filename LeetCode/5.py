eadc15f545ef5a04371cf1eac91fd422faeb8478
Geon-Woo Kim <youlive789@gmail.com>
2020-07-23 11:31:44 +0900

    feat: Modify LeetCode problem 5

--- LeetCode/5.py
+++ LeetCode/5.py
@@ -1,22 +1,36 @@
 class Solution:
 
     def longestPalindrome(self, s: str) -> str:
+        import sys
+        sys.setrecursionlimit(2000)
         self.cache = [[0] * (len(s)+1)] * (len(s)+1)
         return self.answer(s, 0, len(s))
 
     def answer(self, target, start, end) ->str:
-        if self.cache[start][end]:
+        # print(start, " ", end, " ", self.cache[start][end], " ", target[start:end])
+        if self.cache[start][end] != 0:
             return self.cache[start][end]
-        else :
-            if len(target) <= 1:
+        else:
+            if len(target) == 1:
                 self.cache[start][end] = target
                 return target
             else:
                 if self.is_palindrome(target):
                     self.cache[start][end] = target
                     return target
-                left = self.cache[start][end - 1] = self.answer(target[start:end-1], start, end - 1)
-                right = self.cache[start + 1][end] = self.answer(target[start+1:end], start + 1, end)
+
+                if self.cache[start][end-1] != 0:
+                    left = self.cache[start][end-1]
+                else:
+                    left = self.cache[start][end-1] = self.answer(target[start:end-1], start, end - 1)
+
+                if self.cache[start+1][end] != 0:
+                    right = self.cache[start+1][end]
+                else:
+                    right = self.cache[start+1][end] = self.answer(target[start+1:end], start + 1, end)
+
+                print(left, " ", right , " ==> ", target[start:end-1], " ", target[start+1:end])
+
                 if len(left) > len(right):
                     return left
                 else:
@@ -35,6 +49,6 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
 
-    # print(s.longestPalindrome("babac"))
-    # print(s.longestPalindrome("ccb"))
-    print(s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
+    # print(s.longestPalindrome("babcb"))
+    print(s.longestPalindrome("eabcb"))
+    # print(s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
