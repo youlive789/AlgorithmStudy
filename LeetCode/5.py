621b444e0a215f3c5adc5637d6ed3d5865f9141a
Geon-Woo Kim <youlive789@gmail.com>
2020-07-23 09:36:24 +0900

    feat: Modify leetCode problem 5

--- LeetCode/5.py
+++ LeetCode/5.py
@@ -1,20 +1,26 @@
 class Solution:
+
     def longestPalindrome(self, s: str) -> str:
+        self.cache = [[0] * (len(s)+1)] * (len(s)+1)
         return self.answer(s, 0, len(s))
 
     def answer(self, target, start, end) ->str:
-        if len(target) <= 1:
-            return target
-        else:
-            if self.is_palindrome(target):
+        if self.cache[start][end]:
+            return self.cache[start][end]
+        else :
+            if len(target) <= 1:
+                self.cache[start][end] = target
                 return target
-            left = self.answer(target[start:end-1], start, end - 1)
-            right = self.answer(target[start+1:end], start + 1, end)
-
-            if len(left) > len(right):
-                return left
             else:
-                return right
+                if self.is_palindrome(target):
+                    self.cache[start][end] = target
+                    return target
+                left = self.cache[start][end - 1] = self.answer(target[start:end-1], start, end - 1)
+                right = self.cache[start + 1][end] = self.answer(target[start+1:end], start + 1, end)
+                if len(left) > len(right):
+                    return left
+                else:
+                    return right
 
     def is_palindrome(self, target):
         mid = int(len(target) / 2)
