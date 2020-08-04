c1f3311f519581e4961a5c16ba3fc3b5bc576283
gwkim <youlive789@gmail.com>
2020-08-04 14:18:10 +0900

    feat: Fails backjoon 2212 problem

--- BackJoon/greedy/2212.cpp
+++ BackJoon/greedy/2212.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
-#include <set>
-
+#include <algorithm>
 using namespace std;
-
-int main() {
-
+ 
+int sensor[10000];
+int dist[999];
+int result;
+ 
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+ 
     int N, K;
     cin >> N >> K;
-
-    set<int> container;
-    for (int idx = 0; idx < N; idx++) {
-        int tmp;
-        cin >> tmp;
-        container.insert(tmp);
-    }
-
+ 
+    for (int i = 0; i < N; i++)
+        cin >> sensor[i];
+ 
+    sort(sensor, sensor + N);
+ 
+    // 인접한 센서 사이의 거리
+    for (int i = 0; i < N - 1; i++)
+        dist[i] = sensor[i + 1] - sensor[i];
+ 
+    // 거리 내림차순 정렬
+    sort(dist, dist + (N - 1), greater<int>());
+ 
+    // 거리가 먼 순서대로 삭제
+    for (int i = 0; i < K - 1; i++) dist[i] = 0;
+ 
+    for (int i = 0; i < N - 1; i++) result += dist[i];
+ 
+    cout << result;
+ 
     return 0;
 }
