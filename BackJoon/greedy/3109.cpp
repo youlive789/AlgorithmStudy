bbc59352dd4f218fab2030a36a73e32311b94142
gwkim <youlive789@gmail.com>
2020-08-12 17:24:32 +0900

    feat: modify backjoon greedy 3109

--- BackJoon/greedy/3109.cpp
+++ BackJoon/greedy/3109.cpp
@@ -2,55 +2,35 @@
 
 using namespace std;
 
-unsigned short cache[10001][501];
+int answer = 0;
 char pipemap[10001][501];
+bool visited[10001][501];
 
-int searchPipe(int row, int col, int endRow, int endCol, int lastDirection, bool fail) {
+void searchPipe(int row, int col, int endRow, int endCol) {
     
-    if (cache[row][col]) {
-        return cache[row][col];
+    if (col >= endCol || row >= endRow) {
+        return;
     }
-    else {
-        if (col >= endCol || row >= endRow || col < 0 || row < 0) {
-            return 0;
-        }
-        if (col == endCol - 1 && row < endRow ) {
-            return 1;
-        }
 
-        unsigned short answer = 0;
+    if (col == endCol - 1 && row < endRow ) {
+        answer++;
+        return;
+    }
 
-        bool canNotGoRightUp = fail && (lastDirection == 1);
-        bool canNotGoRight = fail && (lastDirection == 2);
-        bool canNotGoRightDown = fail && (lastDirection == 3);
-
-        // 오른쪽 위
-        if (pipemap[row - 1][col + 1] == '.' && !canNotGoRightUp) {
-            pipemap[row - 1][col + 1] = 'x';
-            answer += searchPipe(row - 1, col +1, endRow, endCol, 1, false);
-        }
-        // 오른쪽
-        else if (pipemap[row][col + 1] == '.' && !canNotGoRight) {
-            pipemap[row][col + 1] = 'x';
-            answer += searchPipe(row, col +1, endRow, endCol, 2, false);
-        }
-        // 오른쪽 아래
-        else if (pipemap[row + 1][col + 1] == '.' && !canNotGoRightDown) {
-            pipemap[row + 1][col + 1] = 'x';
-            answer += searchPipe(row + 1, col +1, endRow, endCol, 3, false);
-        }
-        else {
-            pipemap[row][col] = '.';
-            switch (lastDirection)
-            {
-                case 1: answer += searchPipe(row + 1, col -1, endRow, endCol, 1, true); break;
-                case 2: answer += searchPipe(row, col -1, endRow, endCol, 2, true); break;
-                case 3: answer += searchPipe(row - 1, col -1, endRow, endCol, 3, true); break;
-                default: break;
-            }
-        }
-
-        return cache[row][col] = answer;
+    // 오른쪽 위
+    if (pipemap[row - 1][col + 1] == '.' && !visited[row-1][col+1]) {
+        pipemap[row-1][col+1] = 'x';
+        searchPipe(row - 1, col +1, endRow, endCol);
+    }
+    // 오른쪽
+    else if (pipemap[row][col + 1] == '.' && !visited[row][col+1]) {
+        pipemap[row][col+1] = 'x';
+        searchPipe(row, col +1, endRow, endCol);
+    }
+    // 오른쪽 아래
+    else if (pipemap[row + 1][col + 1] == '.' && !visited[row+1][col+1]) {
+        pipemap[row+1][col+1] = 'x';
+        searchPipe(row + 1, col +1, endRow, endCol);
     }
 }
 
@@ -67,9 +47,8 @@ int main() {
         }
     }
 
-    unsigned short answer = 0;
     for (int rowIndex = 0; rowIndex < row; rowIndex++) {
-        answer += searchPipe(rowIndex, 0, row, col, 0, false);
+        searchPipe(rowIndex, 0, row, col);
     }
 
     cout << answer << endl;
