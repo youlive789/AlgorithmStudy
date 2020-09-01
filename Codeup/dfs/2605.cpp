f10190e015158a38a7cd2005eb61ac399c7a7ecf
gwkim <youlive789@gmail.com>
2020-09-01 10:45:03 +0900

    feat: Practice binary search

--- Codeup/dfs/2605.cpp
+++ Codeup/dfs/2605.cpp
@@ -5,33 +5,19 @@ using namespace std;
 int grid[7][7];
 
 int answer = 0;
-int dx[8] = {-1,0,1,1,1,0,-1,-1};
-int dy[8] = {-1,-1,-1,0,1,1,1,0};
 
-void findArea(int x, int y, int count, int last) {
+int findArea(int x, int y, int color) {
     // 기저사례: 격자를 벗어났다면
-    if (x < 0 || y < 0 || x > 7 || y > 7) return;
-
-    if (grid[y][x] == -1) return;
+    if (x < 0 || y < 0 || x >= 7 || y >= 7) return 0;
 
     // 기저사례: 현재 위치가 이전 위치와 다르다면
-    if (grid[y][x] != last) return;
+    if (grid[y][x] != color) return 0;
 
-    // 기저사례: count == 2 이면서 현재 위치가 last와 같다면 (정답)
-    if (count == 2 && grid[y][x] == last) {
-        cout << count << " " << last << endl;
-        answer++;
-        return;
-    }
+    // 기저사례: 같은 곳을 탐색하지 못하게 0으로 만듬
+    if (grid[y][x] == color) grid[y][x] = 0;
 
-    int now = grid[y][x];
-
-    // 8방향을 순회
-    for (int idx = 0; idx < 8; idx++) {
-        grid[y][x] = -1;
-        findArea(x + dx[idx], y + dy[idx], count + 1, now);
-        grid[y][x] = now;
-    }
+    // 4 방향을 순회
+    return 1 + findArea(x-1, y, color) + findArea(x+1, y, color) + findArea(x, y+1, color) + findArea(x, y-1, color);
 }
 
 int main() {
@@ -46,7 +32,11 @@ int main() {
 
     for (int y = 0; y < 7; y++) {
         for (int x = 0; x < 7; x++) {
-            findArea(x, y, 0, grid[y][x]);
+            int tmp = 0;
+            if (grid[y][x] != 0) {
+                tmp = findArea(x, y, grid[y][x]);
+            }
+            if (tmp >= 3) answer++;
         }
     }
 
