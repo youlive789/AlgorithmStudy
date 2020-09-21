ff7cb0d4aeb8441f506ca971803671c452ef8954
gwkim <youlive789@gmail.com>
2020-09-22 08:59:26 +0900

    feat: Solved backjoon 14501 python

--- BackJoon/bruteforce/14501.py
+++ BackJoon/bruteforce/14501.py
@@ -1,5 +1,18 @@
 import sys
 
+def find_maxcost(now_time, now_cost, time_table, cost_table):
+    if now_time > len(time_table):
+        return 0
+
+    if now_time == len(time_table):
+        return now_cost
+
+    go = find_maxcost(now_time + time_table[now_time], now_cost + cost_table[now_time], time_table, cost_table)
+    notgo = find_maxcost(now_time + 1, now_cost, time_table, cost_table)
+    result = max(go, notgo)
+
+    return result
+
 if __name__ == "__main__":
     cases = int(sys.stdin.readline())
     
@@ -10,4 +23,5 @@ if __name__ == "__main__":
         time_table.append(time)
         cost_table.append(cost)
 
-    
+    result = find_maxcost(0, 0, time_table, cost_table)
+    print(result)
