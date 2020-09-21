60bab9ab3b938bb290d109bdb93eff327e9c04b1
gwkim <youlive789@gmail.com>
2020-09-21 11:14:56 +0900

    feat: solved backjoon 1436 python

--- BackJoon/bruteforce/1436.py
+++ BackJoon/bruteforce/1436.py
@@ -1,4 +1,17 @@
 import sys
 
 if __name__ == "__main__":
-    pass
+    target = int(sys.stdin.readline())
+
+    index = 0
+    result = 0
+    for i in range(999999999):
+        if "666" in str(i) :
+            index += 1
+
+        if index == target:
+            result = i
+            break
+
+    print(result)
+
