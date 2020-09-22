ce7dc1bac53014e622e995fc78b72ab0ec90f27d
gwkim <youlive789@gmail.com>
2020-09-22 11:14:21 +0900

    feat: Finished binarytree scratch

--- scratch/binarytree.cpp
+++ scratch/binarytree.cpp
@@ -5,11 +5,27 @@ int main() {
 
     BinaryTree* bt = new BinaryTree();
 
-    for (int idx = 0; idx < 5; idx++) {
+    for (int idx = 0; idx < 10; idx++) {
         bt->insertNode(idx);
     }
 
-    bt->printTree();
+    std::cout << "preorder" << std::endl;
+    bt->printTree(0); // preorder
+    std::cout << std::endl;
+    // result : 0 1 3 7 8 4 9 2 5 6
+
+    std::cout << "inorder" << std::endl;
+    bt->printTree(1); // inorder
+    std::cout << std::endl;
+    // result : 7 3 8 1 9 4 0 5 2 6
+
+    std::cout << "postorder" << std::endl;
+    bt->printTree(2); // postorder
+    std::cout << std::endl;
+    // result : 7 8 3 9 4 1 5 6 2 0
+
+    Node* found = bt->findNode(9);
+    std::cout << found->value << std::endl;
 
     return 0;
 }
--- scratch/datastructure/BinaryTree.h
+++ scratch/datastructure/BinaryTree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <queue>
 
 class Node {
 public:
@@ -13,15 +14,80 @@ class BinaryTree {
 private:
     Node* head;
 
-    void insert(Node* cursor, const int& value) {
-        // queue를 이용해서 inorder 순서로 삽입하기
+    void insert(Node* node, const int& value) {
+
+        std::queue<Node*> q;
+        q.push(node);
+
+        Node* cursor;
+        while (!q.empty()) {
+            cursor = q.front();
+            q.pop();
+
+            if (cursor->left != nullptr) {
+                q.push(cursor->left);
+            }
+            else {
+                cursor->left = new Node(value);
+                break;
+            }
+            
+            if (cursor->right != nullptr) {
+                q.push(cursor->right);
+            }
+            else {
+                cursor->right = new Node(value);
+                break;
+            }
+        }
+        return;
     }
 
-    void preorderTraversal(Node* cursor) {
+    Node* findPreorderTraversal(Node* cursor, const int& value) {
         if (cursor != nullptr) {
-            std::cout << cursor->value << std::endl;
-            this->preorderTraversal(cursor->left);
-            this->preorderTraversal(cursor->right);
+            if (cursor->value == value) {
+                return cursor;
+            }
+            
+            Node* left = this->findPreorderTraversal(cursor->left, value);
+            Node* right = this->findPreorderTraversal(cursor->right, value);
+
+            if (left != nullptr && right == nullptr) {
+                return left;
+            }
+            else if (left == nullptr && right != nullptr) {
+                return right;
+            }
+            else {
+                return nullptr;
+            }
+        }
+        else {
+            return nullptr;
+        }
+    }
+
+    void printPreorderTraversal(Node* cursor) {
+        if (cursor != nullptr) {
+            std::cout << cursor->value << " ";
+            this->printPreorderTraversal(cursor->left);
+            this->printPreorderTraversal(cursor->right);
+        }
+    }
+
+    void printInorderTraversal(Node* cursor) {
+        if (cursor != nullptr) {
+            this->printInorderTraversal(cursor->left);
+            std::cout << cursor->value << " ";
+            this->printInorderTraversal(cursor->right);
+        }
+    }
+
+    void printPostorderTraversal(Node* cursor) {
+        if (cursor != nullptr) {
+            this->printPostorderTraversal(cursor->left);
+            this->printPostorderTraversal(cursor->right);
+            std::cout << cursor->value << " ";
         }
     }
 
@@ -29,12 +95,24 @@ public:
     BinaryTree() {}
 
     void insertNode(const int& value) {
+        if (this->head == nullptr) {
+            this->head = new Node(value);
+            return;
+        }
         this->insert(this->head, value);
     }
 
-    void printTree() {
-        this->preorderTraversal(this->head);
+    void printTree(const int& order) {
+        switch (order) {
+            case 0: this->printPreorderTraversal(this->head); break;
+            case 1: this->printInorderTraversal(this->head); break;
+            case 2: this->printPostorderTraversal(this->head); break;
+            default: break;
+        }
+        std::cout << std::endl;
     }
 
-    Node* findNode(const int& value) {}
+    Node* findNode(const int& value) {
+        return this->findPreorderTraversal(this->head, value);
+    }
 };
