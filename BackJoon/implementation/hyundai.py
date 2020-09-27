733c2ee73e784d4fc81bfaf3d5e45475d977fe13
gwkim <youlive789@gmail.com>
2020-09-27 19:15:55 +0900

    feat: Solved hyundai.

--- BackJoon/implementation/hyundai.py
+++ BackJoon/implementation/hyundai.py
@@ -18,23 +18,18 @@ import sys
 def cyclone(size, clockwise):    
     start_position = {
         True: [[0, 0], [0, size - 1], [size - 1, size - 1], [size - 1, 0]],
-        False: [[0, size - 1], [size -1, size - 1], [size - 1, 0], [0, 0]],
+        False: [[0, size - 1], [0, 0], [size - 1, 0], [size -1, size - 1]],
     }
 
     direction = {
         True: [[0, 1], [1, 0], [0, -1], [-1, 0]],
-        False: [[0, -1], [-1, 0], [0, 1], [1, 0]],
+        False: [[0, -1], [1, 0], [0, 1], [-1, 0]],
     }
 
-    even_center_position = {
-        True: [[size // 2, size // 2], [size // 2, (size // 2) - 1], [(size // 2) - 1, (size // 2) - 1], [(size // 2) - 1, size // 2]],
-        False: [[size // 2, (size // 2) - 1], [(size // 2) - 1, (size // 2) - 1], [(size // 2) - 1, (size // 2)], [size // 2, size // 2]],
-    }
-
-    answer = make_grid(size, clockwise, start_position, direction, even_center_position)
+    answer = make_grid(size, clockwise, start_position, direction)
     return answer
 
-def make_grid(size, clockwise, start_position, direction, even_center_position):
+def make_grid(size, clockwise, start_position, direction):
 
     answer = [[0 for _ in range(size)] for _ in range(size)]
 
@@ -43,11 +38,11 @@ def make_grid(size, clockwise, start_position, direction, even_center_position):
     check = 1
 
     row, col = start_position[clockwise][idx_start][0], start_position[clockwise][idx_start][1]
+    even_center_position = [size // 2, (size // 2) - 1]
 
     while True:
 
         answer[row][col] = check
-        print(answer)
         cnt_direction += 1
         check += 1
 
@@ -55,7 +50,7 @@ def make_grid(size, clockwise, start_position, direction, even_center_position):
         if idx_start == 3:
             if size % 2 == 1 and row == size // 2 and col == size // 2:
                 break
-            elif size % 2 == 0 and row == even_center_position[clockwise][idx_start][0] and col == even_center_position[clockwise][idx_start][1]:
+            elif size % 2 == 0 and row in even_center_position and col in even_center_position:
                 break
 
         # 중앙 지점에 도달했다면
@@ -68,7 +63,7 @@ def make_grid(size, clockwise, start_position, direction, even_center_position):
                 row, col = start_position[clockwise][idx_start][0], start_position[clockwise][idx_start][1]
                 continue
         else:
-            if row == even_center_position[clockwise][idx_start][0] and col == even_center_position[clockwise][idx_start][1]:
+            if row in even_center_position and col in even_center_position:
                 idx_start += 1
                 check, cnt_direction = 1, 0
                 limit_direction = size - 1
@@ -79,12 +74,13 @@ def make_grid(size, clockwise, start_position, direction, even_center_position):
         if cnt_direction == limit_direction:
             idx_direction += 1
             if idx_direction == 4:
-                idx_direction == 0
+                idx_direction = 0
 
-            if limit_direction == 2:
+            if limit_direction <= 2:
                 limit_direction -= 1
             else:
                 limit_direction -= 2
+            cnt_direction = 0
         
         row += direction[clockwise][idx_direction][0]
         col += direction[clockwise][idx_direction][1]
@@ -93,5 +89,15 @@ def make_grid(size, clockwise, start_position, direction, even_center_position):
 
 if __name__ == "__main__":
     size = int(sys.stdin.readline())
-    print(cyclone(3, True))
+    print(cyclone(size, True))
+    print()
+    print(cyclone(size, False))
 
+"""
+[
+    [1, 2, 3, 1], 
+    [3, 0, 4, 2], 
+    [2, 4, 4, 3], 
+    [1, 3, 2, 1]
+]
+"""
