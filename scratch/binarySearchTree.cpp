0dbd73df641d127f7253a0391cb8c602d0c32cdc
gwkim <youlive789@gmail.com>
2020-09-28 17:31:42 +0900

    feat: Finished binary search tree

--- scratch/binarySearchTree.cpp
+++ scratch/binarySearchTree.cpp
@@ -4,14 +4,27 @@
 int main() {
 
     BinarySearchTree* bst = new BinarySearchTree();
-    bst->insert(1);
-    bst->insert(2);
-    bst->insert(3);
     bst->insert(4);
     bst->insert(5);
+    bst->insert(6);
+    bst->insert(3);
+    bst->insert(2);
+    bst->insert(1);
+    bst->insert(1); // 이미 있는 값입니다.
+
+    std::cout << bst->find(3)->value << " 찾았습니다 !" << std::endl;
 
     bst->printTree();
-    std::cout << bst->find(3) << std::endl;
+    bst->remove(3);
+    bst->printTree();
+    bst->remove(1);
+    bst->printTree();
+    bst->remove(4);
+    bst->printTree();
+    bst->remove(2);
+    bst->printTree();
+    bst->remove(6);
+    bst->printTree();
 
     return 0;
 }
