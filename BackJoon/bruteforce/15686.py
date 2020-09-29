ed07a60a94b6d6a2425a328af80bf28cd3125878
gwkim <youlive789@gmail.com>
2020-09-29 11:26:54 +0900

    feat: Failed Backjoon 15686 python

--- BackJoon/bruteforce/15686.py
+++ BackJoon/bruteforce/15686.py
@@ -1,46 +1,40 @@
 import sys
+import math
 from itertools import combinations
 
+def get_distance(comb_chicken, house):
+    dist = 0
+    for row_house, col_house in house:
+        house_dist, now_house_dist = math.inf, 0
+        for row_chicken, col_chicken in comb_chicken:
+            now_house_dist = abs(row_house - row_chicken) + abs(col_house - col_chicken)
+            if now_house_dist < house_dist:
+                house_dist = now_house_dist
+        dist += house_dist
+    return dist
+
 if __name__ == "__main__":
-    MAX_VAL = 99999999
-    size, choose = list(map(int, sys.stdin.readline().split(" ")))
+    
+    # 집과 치킨집의 위치정보를 저장한다.
+    house, chicken = [], []
+    N, M = list(map(int, sys.stdin.readline().split(" ")))
+    for row in range(N):
+        position = list(map(int, sys.stdin.readline().split(" ")))
+        for col in range(N):
+            if position[col] == 1:
+                house.append([row, col])
+            elif position[col] == 2:
+                chicken.append([row, col])
 
-    city = []
-    for _ in range(size):
-        city.append(list(map(int, sys.stdin.readline().split(" "))))
-
-    chicken = []
-    distance_table = {}
-    for row in range(size):
-        for col in range(size):
-            if city[row][col] == 1:
-                distance_table[str(row)+str(col)] = MAX_VAL
-            elif city[row][col] == 2:
-                chicken.append(str(row)+str(col))
-
-
-    result = MAX_VAL
-    for i in range(1, choose + 1):
-        chicken_comb = list(combinations(chicken, i))
-
-        for comb in chicken_comb:
-            now_calc = MAX_VAL
-            for chicken_pos in comb:
-                for house in distance_table:
-                    dist = abs(int(house[0]) - int(chicken_pos[0])) + abs(int(house[1]) - int(chicken_pos[1]))
-                    distance_table[house] = min(distance_table[house], dist)
-
-                now_dist = 0
-                for _, items in distance_table.items():
-                    now_dist += items
-                now_calc = min(now_calc, now_dist)
-
-            result = min(result, now_calc)
-
-            for house in distance_table:
-                distance_table[house] = MAX_VAL
+    comb_chicken = list(combinations(chicken, M))
+    
+    result = math.inf
+    for comb in comb_chicken:
+        dist = get_distance(comb, house)
+        if dist < result:
+            result = dist
 
     print(result)
-            
+
     
 
