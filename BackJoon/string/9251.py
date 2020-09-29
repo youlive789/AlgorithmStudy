4bbdb8168d07da288507c856912cd03461ae115f
gwkim <youlive789@gmail.com>
2020-09-29 10:30:18 +0900

    feat: Failed backjoon 9251 python

--- BackJoon/string/9251.py
+++ BackJoon/string/9251.py
@@ -4,20 +4,13 @@ if __name__ == "__main__":
     a = list(sys.stdin.readline().replace("\n", ""))
     b = list(sys.stdin.readline().replace("\n", ""))
 
-    idx_a = 0
-    idx_b = 0
+    cache = [[0 for _ in range(1001)] for _ in range(1001)]
 
-    res = 0
-    while idx_a < len(a) and idx_b < len(b):
-        if a[idx_a] != b[idx_b]:
-            if idx_b == len(b) - 1:
-                idx_a += 1
+    for idx_a in range(1, len(a) + 1):
+        for idx_b in range(1, len(b) + 1):
+            if a[idx_a - 1] == b[idx_b - 1]:
+                cache[idx_a][idx_b] += cache[idx_a - 1][idx_b - 1] + 1
             else:
-                idx_b += 1
-        else:
-            print(idx_a, idx_b)
-            res += 1
-            idx_a += 1
-            idx_b += 1
-    print()
-    print(res)
+                cache[idx_a][idx_b] = max(cache[idx_a - 1][idx_b], cache[idx_a][idx_b - 1])
+
+    print(cache[len(a)][len(b)])
