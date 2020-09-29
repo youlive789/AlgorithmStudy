762f6881fba8da07379cebebed25613ea783fe3c
gwkim <youlive789@gmail.com>
2020-09-29 13:44:39 +0900

    feat: Faild backjoon 3109 python

--- BackJoon/graph/3109.py
+++ BackJoon/graph/3109.py
@@ -1,36 +1,20 @@
 import sys
 
-result = 0
 move = [[-1, 1], [0, 1], [1, 1]]
 
-def in_table(row, col, row_len, col_len):
-    if row < 0 or col < 0 or row >= row_len or col >= col_len:
-        return False
-    else:
+def put_pipe(row, col, grid):
+
+    grid[row][col] = "x"
+    if col == len(grid[0]) - 1:
         return True
 
-def print_grid(grid):
-    print()
-    for a in grid:
-        for c in a:
-            print(c, end="")
-        print()
-
-def put_pipe(row, col, grid):
-    if col == len(grid[0]) - 1:
-        global result
-        result += 1
-        return
-
-    moved = False
     for dy, dx in move:
         row_next = row + dy
         col_next = col + dx
-        if not moved and in_table(row_next, col_next, len(grid), len(grid[0])) and grid[row_next][col_next] != "x":
-            moved = True
-            grid[row_next][col_next] = "x"
-            # print_grid(grid)
-            put_pipe(row_next, col_next, grid)
+        if 0 <= row_next < len(grid) and 0 <= col_next <= len(grid[0]) and grid[row_next][col_next] != "x":
+            if put_pipe(row_next, col_next, grid):
+                return True
+    return False
     
 if __name__ == "__main__":
     row, col = list(map(int, sys.stdin.readline().split(" ")))
@@ -39,7 +23,9 @@ if __name__ == "__main__":
     for _ in range(row):
         grid.append(list(sys.stdin.readline().replace("\n", "")))
 
+    result = 0
     for r in range(row):
-        put_pipe(r, 0, grid)
+        if put_pipe(r, 0, grid):
+            result += 1
 
     print(result)
