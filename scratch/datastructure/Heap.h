230fd1e4f402e0cb010500c3640d4c10161b1a9b
gwkim <youlive789@gmail.com>
2020-10-19 12:42:40 +0900

    feat: finished implement heap scratch

--- scratch/datastructure/Heap.h
+++ scratch/datastructure/Heap.h
@@ -1,34 +1,86 @@
 #pragma once
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
-template <typename T>
-class Node {
-private:
-    T data;
-    Node* left;
-    Node* right;
-public:
-    Node(T data) : data(data) {}
-    ~Node() {}
-};
+/*
+Reference:
+https://www.geeksforgeeks.org/binary-heap/
+*/
 
 template <typename T>
 class Heap {
 private:
-    Node* root;
-    void heapify() {}
+    std::vector<T> heap;
+
+    // 주어진 인덱스의 부모 인덱스를 가져온다.
+    int _parent(int idx) {
+        return (idx - 1) / 2;
+    }
+
+    // 주어진 인덱스의 왼쪽 인덱스를 가져온다.
+    int _left(int idx) {
+        return (2 * idx + 1);
+    }
+
+    // 주어진 인덱스의 오른쪽 인덱스를 가져온다.
+    int _right(int idx) {
+        return (2 * idx + 2);
+    }
+
+    void _heapify(int idx) {
+        int l = this->_left(idx);
+        int r = this->_right(idx);
+
+        int largest = idx;
+        if (l < heap.size() && heap[l] > heap[idx]) {
+            largest = l;
+        }
+
+        if (r < heap.size() && heap[r] > heap[largest]) {
+            largest = r;
+        }
+
+        if (largest != idx) {
+            std::swap(heap[idx], heap[largest]);
+            this->_heapify(largest);
+        }
+    }
+
 public:
     Heap() {}
 
     void insert(T data) {
-        if (!this->root) {
-            this->root = new Node(data);
-            return;
+        heap.push_back(data);
+        int idx = heap.size() - 1;
+
+        // 부모 인덱스가 자식 인덱스보다 작다면 교환한다.
+        while (idx != 0 && heap[this->_parent(idx)] < heap[idx]) {
+            std::swap(heap[idx], heap[this->_parent(idx)]);
+            idx = this->_parent(idx);
         }
     }
 
-    T top() {
+    void pop() {
+        if (heap.size() <= 0) {
+            std::cout << "힙에 요소가 없습니다." << std::endl;
+            return;
+        }
 
+        // 루트 요소를 맨 뒤로 보내고 heapify를 수행한다.
+        int root = heap[0];
+        heap[0] = heap[heap.size() - 1];
+        heap.pop_back();
+        this->_heapify(0);
+
+        return;
     }
 
-    ~Heap() {}
+    T top() {
+        return heap[0];
+    }
+
+    ~Heap() {
+        delete heap;
+    }
 };
--- scratch/heap.cpp
+++ scratch/heap.cpp
@@ -2,5 +2,18 @@
 #include "datastructure/Heap.h"
 
 int main() {
+
+    Heap<int>* heap = new Heap<int>();
+    heap->insert(3);
+    heap->insert(1);
+    heap->insert(5);
+    heap->insert(2);
+    heap->insert(8);
+
+    std::cout << heap->top() << std::endl; // 8
+
+    heap->pop();
+    std::cout << heap->top() << std::endl; // 5
+
     return 0;
 }
