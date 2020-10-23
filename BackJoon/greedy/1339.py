22307d91ed2a33f064daa9a37fcb109c6013646e
Geon-Woo Kim <youlive789@gmail.com>
2020-10-23 09:14:50 +0900

    Feat: fail

--- BackJoon/greedy/1339.py
+++ BackJoon/greedy/1339.py
@@ -1,13 +1,15 @@
 import sys
+from collections import OrderedDict
 
 if __name__ == "__main__":
     # 단어를 입력받는다.
     word_table = []
     word_count = int(sys.stdin.readline())
+    word_max_length, word_min_length = 0, sys.maxsize
     for _ in range(word_count):
-        word_table.append(sys.stdin.readline().replace("\n", ""))
+        word = sys.stdin.readline().replace("\n", "")
+        word_max_length = max(len(word), word_max_length)
+        word_min_length = min(len(word), word_min_length)
+        word_table.append(word)
 
-    # 단어배열을 길이로 정렬한다.
-    # 배열을 순회하면서 알파벳에 숫자를 매핑한다.
-    # 배열을 순회하면서 매핑된 알파벳에 따라 숫자로 변환한다.
-
+    
