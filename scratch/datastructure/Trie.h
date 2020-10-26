163286dbc1048f60545ed01d16a20d6d987390a1
Geon-Woo Kim <youlive789@gmail.com>
2020-10-26 16:22:50 +0900

    feat: Modify trie datastructure

--- scratch/datastructure/Trie.h
+++ scratch/datastructure/Trie.h
@@ -1,5 +1,34 @@
 #pragma once
+#include <iostream>
+#include <vector>
+
+#define ALPHABET_SIZE 26
+
+class Node {
+public:
+    char _value;
+    bool isEnd;
+    std::vector<Node> container;
+    Node(const char& value) : _value(value) {
+        for (int idx = 0; idx < ALPHABET_SIZE; idx++) {
+            this->container.push_back('a' + idx);
+        }
+    }
+};
 
 class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        this->root = new Node('\0');
+    }
 
+    void insert(const std::string& target) {}
+
+    void insert(const char* target) {}
+
+    void search(const std::string& target) {}
+
+    void search(const char* target) {}
 };
