d621f532117e3cd236b1ca4b4e24ce4e39873b7b
Geon-Woo Kim <youlive789@gmail.com>
2020-11-02 08:58:26 +0900

    feat: failed backjoon 1904 python

---
+++ BackJoon/dp/1904.py
@@ -0,0 +1,21 @@
+import sys
+
+if __name__ == "__main__":
+    """
+    f(1) = 1
+    f(2) = 2, 00 11
+    f(3) = 3, 100 001 111
+    f(4) = 5, 1100 1001 0011 1111 0000
+    f(5) = 8, 11100 11001 10011 00111 11111 00001 10000
+    """
+    number = int(sys.stdin.readline())
+    
+    a = 1
+    b = 2
+    answer = 0
+    for idx in range(3, number + 1):
+        answer = a + b
+        a, b = b, a
+        b = answer
+        
+    print(answer % 15746)
