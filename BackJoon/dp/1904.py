90f590ea75265f4fb32d4fddaabfbe8ced9078a7
Geon-Woo Kim <youlive789@gmail.com>
2020-11-02 17:50:31 +0900

    feat: Solved backjoon 1904 python

--- BackJoon/dp/1904.py
+++ BackJoon/dp/1904.py
@@ -9,13 +9,20 @@ if __name__ == "__main__":
     f(5) = 8, 11100 11001 10011 00111 11111 00001 10000
     """
     number = int(sys.stdin.readline())
-    
+    if number == 1:
+        print(number)
+        exit()
+    elif number == 2:
+        print(number)
+        exit()
+
     a = 1
     b = 2
     answer = 0
     for idx in range(3, number + 1):
         answer = a + b
+        answer %= 15746
         a, b = b, a
         b = answer
         
-    print(answer % 15746)
+    print(answer)
