e27dc26922be3214e584bda19b4ff89b3e7e6d27
gwkim <youlive789@gmail.com>
2020-11-04 14:46:30 +0900

    feat: Solved backjoon 1149 python

--- BackJoon/dp/1149.py
+++ BackJoon/dp/1149.py
@@ -6,17 +6,16 @@ def paint(idx, last_color, rgb, cache):
     if idx == len(rgb):
         return 0
 
-    if cache[idx] != -1:
-        return cache[idx]
+    if cache[idx][last_color] != -1:
+        return cache[idx][last_color]
 
     result = sys.maxsize
     for idx_color, color in enumerate(rgb[idx]):
         if idx_color != last_color:
-            cache[idx] = result
             result = min(result, paint(idx + 1, idx_color, rgb, cache) + color)
-            cache[idx] = -1
 
-    return result
+    cache[idx][last_color] = result
+    return cache[idx][last_color]
 
 if __name__ == "__main__":
     house_number = int(sys.stdin.readline())
@@ -25,5 +24,5 @@ if __name__ == "__main__":
     for _ in range(house_number):
         rgb.append(list(map(int, sys.stdin.readline().split(" "))))
 
-    cache = [-1 for _ in range(len(rgb))]
+    cache = [[-1, -1, -1] for _ in range(len(rgb))]
     print(paint(0, -1, rgb, cache))    
