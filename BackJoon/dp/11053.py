6568c95f0b041e44513928651b77cad7ec475bff
Geon-Woo Kim <youlive789@gmail.com>
2020-11-17 09:02:21 +0900

    feat: Fails 11053 python

--- BackJoon/dp/11053.py
+++ BackJoon/dp/11053.py
@@ -1,28 +1,26 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-def least_common(cnt, idx, last_idx, numbers, cache):
+def least_common(cnt, idx, previous, numbers, cache):
     if idx == len(numbers):
         return cnt
 
-    if cache[idx][last_idx] != -1:
-        return cache[idx][last_idx]
+    if cache[idx] != -1:
+        return cache[idx]
 
-    last = 0
-    if last != -1:
-        last = numbers[last]
+    if previous < numbers[idx]:
+        result = least_common(cnt+1, idx+1, numbers[idx], numbers, cache)
+        result = max(result, least_common(cnt, idx+1, previous, numbers, cache))
+    else:
+        result = least_common(cnt, idx+1, previous, numbers, cache)
 
-    left, right = 0, 0
-    if last < numbers[idx]:
-        left = least_common(cnt+1, idx+1, idx, numbers, cache)
-    right = least_common(cnt, idx+1, last_idx, numbers, cache)
-
-    cache[idx][last_idx] = max(left, right)
-    return cache[idx][last_idx]
+    cache[idx] = result
+    return result
 
 if __name__ == "__main__":
     size = int(sys.stdin.readline())
     numbers = list(map(int, sys.stdin.readline().split(" ")))
-    cache = [[-1 for _ in range(size)] for _ in range(size)]
-
+    cache = [-1 for _ in range(size)]
     print(least_common(0, 0, -1, numbers, cache))
+
+    
