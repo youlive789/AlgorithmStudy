afe1ded01c4576930c528bb7fe49348595fb8015
Geon-Woo Kim <youlive789@gmail.com>
2020-11-23 09:04:16 +0900

    feat: Fail backjoon 11053

--- BackJoon/dp/11053.py
+++ BackJoon/dp/11053.py
@@ -1,26 +1,17 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-def least_common(cnt, idx, previous, numbers, cache):
-    if idx == len(numbers):
-        return cnt
-
-    if cache[idx] != -1:
-        return cache[idx]
-
-    if previous < numbers[idx]:
-        result = least_common(cnt+1, idx+1, numbers[idx], numbers, cache)
-        result = max(result, least_common(cnt, idx+1, previous, numbers, cache))
-    else:
-        result = least_common(cnt, idx+1, previous, numbers, cache)
-
-    cache[idx] = result
-    return result
-
 if __name__ == "__main__":
     size = int(sys.stdin.readline())
     numbers = list(map(int, sys.stdin.readline().split(" ")))
-    cache = [-1 for _ in range(size)]
-    print(least_common(0, 0, -1, numbers, cache))
 
-    
+    answer = 0
+    dp = [0 for _ in range(size)]
+    for i in range(size):
+        now = 0
+        for j in range(i + 1):
+            if numbers[i] > numbers[j]:
+                now = max(now, dp[j])
+        dp[i] = now + 1
+        answer = max(answer, dp[i])
+    print(answer)
