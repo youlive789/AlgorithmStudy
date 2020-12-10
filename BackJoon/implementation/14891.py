0d1587777ac3a20fdc571eae6c0d7562fcbe7108
gwkim <youlive789@gmail.com>
2020-12-10 09:40:25 +0900

    feat: Solved backjoon 14891 python

--- BackJoon/implementation/14891.py
+++ BackJoon/implementation/14891.py
@@ -11,4 +11,46 @@ if __name__ == "__main__":
     for _ in range(rotate_count):
         rotates.append(list(map(int, sys.stdin.readline().split(" "))))
 
-    print(rotates)
+    for number_gear, direction in rotates:
+        have_to_ratate = [0, 0, 0, 0]
+
+        idx_gear = number_gear - 1
+        have_to_ratate[idx_gear] = direction
+
+        # 정방향 기어들의 회전방향을 결정한다.
+        temp_direction = direction
+        idx_last_gear, idx_near_gear = idx_gear, idx_gear
+        while idx_near_gear < 3:
+            idx_near_gear += 1
+
+            temp_direction *= -1
+            if gears[idx_last_gear][2] != gears[idx_near_gear][6]:
+                have_to_ratate[idx_near_gear] = temp_direction
+            else:
+                break
+
+            idx_last_gear = idx_near_gear
+
+        # 역방향 기어들의 회전방향을 결정한다.
+        temp_direction = direction
+        idx_last_gear, idx_near_gear = idx_gear, idx_gear
+        while idx_near_gear > 0:
+            idx_near_gear -= 1
+
+            temp_direction *= -1
+            if gears[idx_last_gear][6] != gears[idx_near_gear][2]:
+                have_to_ratate[idx_near_gear] = temp_direction
+            else:
+                break
+
+            idx_last_gear = idx_near_gear
+
+        # 기어들을 회전시킨다.
+        for idx, direct in enumerate(have_to_ratate):
+            gears[idx].rotate(direct)
+
+    answer = 0
+    for idx, gear in enumerate(gears):
+        answer += (2**idx) * gear[0]
+
+    print(answer)
