b623f26ebf62bbb75a4594a99bbf6cc129fdc651
gwkim <youlive789@gmail.com>
2020-12-18 10:30:08 +0900

    feat: Solved backjoon 1021 python

--- BackJoon/datastructure/1021.py
+++ BackJoon/datastructure/1021.py
@@ -1,31 +1,24 @@
-import sys
 from collections import deque
 
-if __name__ == "__main__":
-    queue_size, target_count = list(map(int, list(sys.stdin.readline().split(" "))))
-    targets = list(map(int, list(sys.stdin.readline().split(" "))))
-    
-    q = deque(list(map(lambda x: x+1, list(range(queue_size)))))
-    q_half = queue_size // 2
-    
-    rotate_count = 0
-    for target in targets:
-        while target != q[0]:
-            if q_half > abs(target - q[0]):
-                if target < q[0]:
-                    q.rotate(1)
-                    rotate_count += 1
-                elif target > q[0]:
-                    q.rotate(-1)
-                    rotate_count += 1
-            else:
-                if target > q[0]:
-                    q.rotate(1)
-                    rotate_count += 1
-                elif target < q[0]:
-                    q.rotate(-1)
-                    rotate_count += 1
+q_size, pop_count = list(map(int, input().split(" ")))
+pop_target = list(map(int, input().split(" ")))
 
-        q.popleft()
+q = deque([i for i in range(1, q_size+1)])
+
+answer = 0
+for target in pop_target:
+    target_position = q.index(target)
+    q_half = len(q) // 2
     
-    print(rotate_count)
+    if target_position > q_half:
+        trigger = 1
+    else:
+        trigger = -1
+
+    while q[0] != target:
+        q.rotate(trigger)
+        answer += 1
+
+    q.popleft()
+     
+print(answer)
