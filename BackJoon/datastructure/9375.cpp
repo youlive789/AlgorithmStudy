d420b93e5dde1c5f2017d767a81e7aa1d2d58d66
gwkim <youlive789@gmail.com>
2021-01-15 09:02:41 +0900

    feat: backjoon 9375

--- BackJoon/datastructure/9375.cpp
+++ BackJoon/datastructure/9375.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <set>
 
 using namespace std;
 
 int main() {
 
-    
+    int testCount;
+    cin >> testCount;
+
+    for (int i = 0; i < testCount; i++) {
+        map<string, set<string>> fashion;
+
+        int wearCount;
+        cin >> wearCount;
+        for (int idx = 0; idx < testCount; idx++) {
+            string wear, type;
+            cin >> type;
+
+            if (fashion.find(type) == fashion.end()) {
+                fashion[type] = set<string>();
+            }
+
+            fashion[type].insert(wear);
+        }
+
+        
+        
+    }
+
+
 
     return 0;
 }
