57f25f9bfc20b95b4547aa34077748cf0c2cb3d0
gwkim <youlive789@gmail.com>
2021-01-18 08:54:49 +0900

    feat: Add Backjoon 13305

---
+++ BackJoon/greedy/13305.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    int cityCount;
+    cin >> cityCount;
+
+    // 맨 오른쪽을 제외한 도시에서 가장 최소값 도시를 구한다.
+    // 
+
+    return 0;
+}
