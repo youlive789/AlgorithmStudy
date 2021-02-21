a90d2c25439f82e16d6f4d7a71bea1e9b8f7ec69
Geon-Woo Kim <youlive789@gmail.com>
2021-02-22 08:43:01 +0900

    feat: Solved backjoon 15963

---
+++ BackJoon/implementation/15963.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    unsigned long long n, m;
+    cin >> n >> m;
+
+    if (n == m) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
