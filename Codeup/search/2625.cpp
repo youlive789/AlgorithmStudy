3d8aab5b8e075ce1a5129dd3b600f11035903159
gwkim <youlive789@gmail.com>
2021-02-25 08:50:59 +0900

    feat: add Codeup 2625

---
+++ Codeup/search/2625.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int radius;
+    cin >> radius;
+
+    int answer = 0;
+    for (int i = 1; i < radius - 1; i++) {
+        for (int j = 1; j < radius - i - 1; j++) {
+            
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
