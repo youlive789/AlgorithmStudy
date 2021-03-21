f9e27079409ecfd123eb3cb6b0a72e5adbf084b7
gwkim <youlive789@gmail.com>
2021-03-21 15:01:06 +0900

    feat: Solved backjoon 10757

--- BackJoon/math1/10757.cpp
+++ BackJoon/math1/10757.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+vector<int> stringToNumber(const string& target);
+vector<int> vectorSum(const vector<int>& a, const vector<int>& b);
+void printSum(const vector<int>& numberSum);
 
 vector<int> stringToNumber(const string& target) {
     vector<int> numberVector;
@@ -12,7 +17,7 @@ vector<int> stringToNumber(const string& target) {
     return numberVector;
 }
 
-vector<int> vectorSum(const string& a, const string& b) {
+vector<int> vectorSum(const vector<int>& a, const vector<int>& b) {
     vector<int> numberSum;
 
     int index = 0;
@@ -30,32 +35,47 @@ vector<int> vectorSum(const string& a, const string& b) {
         if (index >= a.size() && index >= b.size()) break;
 
         numberSum.push_back(targetSum);
+        index++;
     }
 
     int idx = 0;
-    while (true)
-        
-
+    while (true) {
         if (numberSum[idx] >= 10) {
-            numberSum[idx+1] += (numberSum[idx] % 10);
+
+            if (idx == numberSum.size() - 1) {
+                numberSum.push_back(0);
+            }
+
+            numberSum[idx+1] += (numberSum[idx] / 10);
             numberSum[idx] %= 10;
         }
+
+        if (idx == numberSum.size() - 1 && numberSum[idx] < 10) break;
         idx++;
     }
+
+    return numberSum;
 }
 
 void printSum(const vector<int>& numberSum) {
-
+    for (int idx = numberSum.size() - 1; idx >=0; --idx) {
+        cout << numberSum[idx];
+    }
+    cout << endl;
 }
 
 int main() {
 
     string a, b;
+    cin >> a >> b;
+    
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
 
     vector<int> aNumber = stringToNumber(a);
     vector<int> bNumber = stringToNumber(b);
 
-    vector<int> numberSum = vectorSum(a, b);
+    vector<int> numberSum = vectorSum(aNumber, bNumber);
     printSum(numberSum);
 
     return 0;
