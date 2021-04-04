8cec5c26309efaacab7ff65b102a331d248e442f
gwkim <youlive789@gmail.com>
2021-04-04 18:19:24 +0900

    feat: Solved backjoon 1969

--- BackJoon/bruteforce/1969.cpp
+++ BackJoon/bruteforce/1969.cpp
@@ -13,26 +13,28 @@ int main() {
     for (int i = 0; i < dnaCount; ++i) {
         std::string tmp;
         std::cin >> tmp;
-        for (auto c : tmp) {
-            if (dnaContainer[i].find(c) != dnaContainer[i].end()) {
-                dnaContainer[i][c]++;
+        for (int idx = 0; idx < dnaLength; ++idx) {
+            if (dnaContainer[idx].find(tmp.at(idx)) != dnaContainer[idx].end()) {
+                dnaContainer[idx][tmp.at(idx)]++;
             }
             else {
-                dnaContainer[i][c] = 1;
+                dnaContainer[idx][tmp.at(idx)] = 1;
             }            
         }
     }
 
     // dna 만들기
     std::string answer;
-    for (int i = 0; i < dnaCount; ++i) {
+    int hammingDistance = 0;
+    for (int i = 0; i < dnaLength; ++i) {
         auto pair 
-            = std::max_element(dnaContainer[i].begin(), dnaContainer[i].end(), [] (const std::pair<char, int> & p1, const std::pair<char, int> & p2) {return p1.second > p2.second;});
-        std::cout << pair->first << std::endl;
+            = std::max_element(dnaContainer[i].begin(), dnaContainer[i].end(), [] (const std::pair<char, int> & p1, const std::pair<char, int> & p2) {return p1.second < p2.second;});
         answer += pair->first;
+        hammingDistance += (dnaCount - pair->second);
     }
 
     std::cout << answer << std::endl;
+    std::cout << hammingDistance << std::endl;
 
     return 0;
 }
