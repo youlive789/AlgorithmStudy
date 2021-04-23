22e6bcfa98c16e6824af7faed0013def8d900c12
gwkim <youlive789@gmail.com>
2021-04-23 09:01:40 +0900

    feat: Fail backjoon 18870

--- BackJoon/sort/18870.cpp
+++ BackJoon/sort/18870.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <map>
 #include <vector>
 #include <algorithm>
@@ -9,6 +10,27 @@ int main() {
 
     int count;
     cin >> count;
+
+    set<int> cord_set;
+    int* cords = new int[count];
+    for (int idx = 0; idx < count; ++idx) {
+        int tmp;
+        cin >> tmp;
+        cords[idx] = tmp;
+        cord_set.insert(tmp);
+    }
+
+    map<int,int> cord_table;
+    set<int>::iterator iter = cord_set.begin();
+    for (int i = 0; i < cord_set.size(); ++i) {
+        cord_table[*iter] = i;
+        advance(iter, 1);
+    }
+
+    for (int i = 0; i < count; ++i) {
+        cout << cord_table[cords[i]] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
