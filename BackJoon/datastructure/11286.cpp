cd7556029728e66aa68e8ba08cc069ca3a5a4c18
gwkim <youlive789@gmail.com>
2021-05-08 10:37:09 +0900

    feat: Fail backjoon 11286

---
+++ BackJoon/datastructure/11286.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+typedef pair<long long, long long> val;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+
+    int count;
+    scanf("%d", &count);
+
+    priority_queue<val, vector<val>, greater<val>> q;
+
+    while (count--) {
+        long long number;
+        scanf("%lld", &number);
+
+        if (number != 0) {
+            val value = make_pair(abs(number), number);
+            q.push(value);
+        }
+        else {
+            if (q.empty()) {
+                printf("0");
+            }
+            else {
+                printf("%lld", q.top().second);
+                q.pop();
+            }
+        }
+    }
+
+    return 0;
+}
