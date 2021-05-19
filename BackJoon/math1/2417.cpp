0103b730d517ba0b4cde98e2eeb07043908662c1
gwkim <youlive789@gmail.com>
2021-05-19 10:40:27 +0900

    feat: Solved backjoon 2417

---
+++ BackJoon/math1/2417.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    unsigned long long input;
+    cin >> input;
+
+    cout << (unsigned long long)ceil(sqrt(input)) << endl;
+
+    return 0;
+}
