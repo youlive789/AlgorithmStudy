fb3b97d39302f47f038f462fb7c20ad64f31fd80
gwkim <youlive789@gmail.com>
2021-08-04 08:30:32 +0900

    Feat: Fail backjoon 10174

---
+++ BackJoon/string/10174.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int caseCount;
+    scanf("%d\n", &caseCount);
+
+    while (caseCount--) {
+        string target;
+        getline(cin, target);
+        transform(target.begin(), target.end(), target.begin(), ::toupper);
+
+        bool isPalindrome = true;
+        int targetLength = target.size();
+        for (int idx = 0; idx < targetLength / 2; ++idx) {
+            if (target[targetLength-idx-1] != target[idx]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
